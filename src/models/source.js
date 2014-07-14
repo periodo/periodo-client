cfa35601cf881e53c27a8e3312b702c06034d6be
Patrick Golden <ptgolden0@gmail.com>
2014-07-14 15:57:17 -0400

    Fetch and parse turtle output from Worldcat & CrossRef
    This was a frustrating one.
    
    First off, I create the turtle URLs by hand because of two different
    issues. The simpler one is that WorldCat hasn't yet enabled CORS for
    their Turtle resources. The more complex one is that web browsers act
    differently when they receive a 303 SEE OTHER response. Seeing a CORS
    request, Firefox will preserve the `Accept` header. Chrome does not.
    There's no way to change the redirect request either- the redirection is
    done transparently by the browser.
    
    Chrome will be fixed in version 36 [1], and WorldCat will probably come
    around on the CORS front soon. Blah
    
    Otherwise, this is all pretty neat. The turtle triples are parsed and
    stored using the N3 library, and then we pluck out the data that we need
    (title, date, authorship) and set it on the source model. Some of the
    code calls for a refactor or splitting out of components, but that can
    come later.
    
    [1] https://code.google.com/p/chromium/issues/detail?id=162183

--- src/models/source.js
+++ src/models/source.js
@@ -1,60 +1,56 @@
 "use strict";
 
-var _ = require('underscore')
-  , Backbone = require('../backbone')
+var Backbone = require('../backbone')
   , CreatorCollection = require('../collections/creator')
+  , parseSourceLD = require('../utils/source_ld_parser')
   , stringify = require('json-stable-stringify')
   , stringifyOpts = {
     space: '  ',
     cmp: function (a, b) { return a.key > b.key ? 1 : -1; },
   }
 
-var BASE_URL = 'http://ptgolden.org/oclc/'
-
-function formatContrib(graph, contribId) {
-  var contrib = _(graph).findWhere({ '@id': contribId })
-    , data = {}
-    , name
-
-  data['@id'] = contribId;
-  if ('schema:name' in contrib) {
-    name = contrib['schema:name'];
-    if (_.isArray(name)) name = name[0];
-    data['name'] = name
-  }
-  return data;
-}
+var WORLDCAT_REGEX = /www.worldcat.org\/oclc\/(\d+)/
+  , DXDOI_REGEX = /dx.doi.org\/(.*)/
 
 module.exports = Backbone.Model.extend({
   idAttribute: '@id',
   initialize: function () {
     this.contributors = new CreatorCollection();
     this.creators = new CreatorCollection();
+    global.z = this;
   },
   url: function () {
-    return BASE_URL + this.get('@id').match(/\d+$/);
+    var uri = this.get('@id') || ''
+
+    if (uri.match(WORLDCAT_REGEX)) {
+      return 'http://ptgolden.org/oclc/' + uri.match(WORLDCAT_REGEX)[1];
+      //return 'http://experiment.worldcat.org/oclc/' + uri.match(WORLDCAT_REGEX)[1] + '.ttl';
+    } else if (uri.match(DXDOI_REGEX)) {
+      return 'http://data.crossref.org/' + global.encodeURIComponent(uri.match(DXDOI_REGEX)[1]);
+    }
+  },
+  fetchLD: function () {
+    var that = this;
+
+    return this.sync('read', this, {
+      dataType: 'text',
+      accepts: { text: 'text/turtle' }
+    }).then(parseSourceLD.bind(null, that.id)).done(function (data) {
+      data = that.parse(data);
+      that.set(data);
+      // TODO: Trigger events here
+    });
   },
   parse: function (resp) {
-    var data = {}
-      , graph = resp['@graph']
-      , item = _(graph).findWhere({ '@id': this.id })
-
-    // Need year published, title, creators, contributors, and PartOf
-    if ('schema:datePublished' in item) {
-      data.yearPublished = item['schema:datePublished'];
+    if ('contributors' in resp) {
+      this.contributors.set(resp.contributors);
+      delete resp.contributors;
     }
-
-    if ('schema:name' in item) {
-      data.title = item['schema:name']['@value'];
+    if ('creators' in resp) {
+      this.creators.set(resp.creators);
+      delete resp.creators;
     }
-
-    if ('contributor' in item) {
-      this.contributors.set([].concat(item.contributor).map(formatContrib.bind(null, graph)));
-    }
-    if ('creator' in item) {
-      this.creators.set([].concat(item.creator).map(formatContrib.bind(null, graph)));
-    }
-    return data;
+    return resp;
   },
   toJSONLD: function () {
     var obj = this.toJSON();
---
+++ src/utils/source_ld_parser.js
@@ -0,0 +1,90 @@
+"use strict";
+
+var $ = require('jquery')
+  , _ = require('underscore')
+  , N3 = require('n3')
+
+var DEFAULT_PREDICATES = {
+  title: ['http://purl.org/dc/terms/title', 'schema:name'],
+  yearPublished: ['http://purl.org/dc/terms/date', 'schema:datePublished'],
+  creators: ['http://purl.org/dc/terms/creator', 'schema:creator'],
+  contributors: ['http://purl.org/dc/terms/contributor', 'schema:contributor'],
+  name: ['http://xmlns.com/foaf/0.1/name', 'schema:name']
+}
+
+/*
+ * Parsers for worldcat and citeseer citations. Returns the following
+ * attributes, if they exist:
+ *   title, yearPublished, creators, contributors, and partOf
+ */
+
+
+/*
+ * Returns Promise that resolves with an N3.store
+ */
+function parseTurtle(turtle) {
+  var parser = N3.Parser()
+    , store = N3.Store()
+    , dfd = $.Deferred()
+
+  parser.parse(turtle, function (error, triple, prefixes) {
+    if (triple) {
+      store.addTriple(triple);
+    }
+    else {
+      store.addPrefixes(prefixes);
+      dfd.resolve(store);
+    }
+  });
+
+  return dfd.promise();
+}
+
+function getFirstMatchingPredicate(store, subject, predicates) {
+  var matchingObjects;
+  predicates = _.isArray(predicates) ? predicates : Array.prototype.slice.call(arguments, 2);
+  for (var i = 0; i < predicates.length; i++) {
+    matchingObjects = store.find(subject, predicates[i], null);
+    if (matchingObjects.length) break;
+  }
+  return matchingObjects;
+}
+
+function getFirstLiteralObject(store, subject, predicates) {
+  return _.chain(getFirstMatchingPredicate.apply(null, arguments))
+    .pluck('object')
+    .map(N3.Util.getLiteralValue)
+    .first()
+    .value();
+}
+
+function formatContrib(store, entity) {
+  return {
+    '@id': entity,
+    'name': getFirstLiteralObject(store, entity, DEFAULT_PREDICATES.name)
+  }
+}
+
+function makeSourceRepr(entity, store) {
+  var data = {}
+
+  data.title = getFirstLiteralObject(store, entity, DEFAULT_PREDICATES.title);
+
+  data.yearPublished = getFirstLiteralObject(store, entity, DEFAULT_PREDICATES.yearPublished);
+
+  data.creators = _.chain(getFirstMatchingPredicate(store, entity, DEFAULT_PREDICATES.creators))
+    .pluck('object')
+    .map(formatContrib.bind(null, store))
+    .value();
+
+  data.contributors = _.chain(getFirstMatchingPredicate(store, entity, DEFAULT_PREDICATES.contributors))
+    .pluck('object')
+    .map(formatContrib.bind(null, store))
+    .value();
+
+  return data;
+}
+
+module.exports = function (entity, ttl) {
+  return parseTurtle(ttl).then(makeSourceRepr.bind(null, entity));
+}
