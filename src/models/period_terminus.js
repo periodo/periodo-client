f270b4e1ca3f8753e3b9cc4ea5b735e6e440e6cd
Patrick Golden <ptgolden0@gmail.com>
2014-07-26 18:10:15 -0400

    Properly serialized period.{start,stop}

--- src/models/period_terminus.js
+++ src/models/period_terminus.js
@@ -23,5 +23,13 @@ module.exports = Backbone.RelationalModel.extend({
     ret.label = data.label;
     ret = _.extend(ret, data.in || {});
     return ret;
+  },
+  toJSON: function () {
+    return {
+      label: this.get('label'),
+      'in': {
+        year: this.get('year')
+      }
+    }
   }
 });
