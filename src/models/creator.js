7fc4986e31294f0cd8cae890e7a47b8ed5ec95a3
Patrick Golden <ptgolden0@gmail.com>
2014-07-29 17:24:49 -0400

    Generate skolem IRI as ID for creators without IDs
    This is done in toJSON so that "blank node" creators without IDs can be
    saved to IndexedDB.
    
    When we implement a toJSON method that will generate the kind of JSON-LD
    that the dataset uses, we'll have to remove these.

--- src/models/creator.js
+++ src/models/creator.js
@@ -1,7 +1,15 @@
 "use strict";
 
 var Backbone = require('../backbone')
+  , genid = require('../utils/generate_skolem_id')
 
 module.exports = Backbone.RelationalModel.extend({
-  fake: false
+  fake: false,
+  toJSON: function () {
+    var ret = Backbone.RelationalModel.prototype.toJSON.apply(this, arguments);
+    if (!this.id) {
+      ret[this.idAttribute] = genid();
+    }
+    return ret;
+  }
 });
