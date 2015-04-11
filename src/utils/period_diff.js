9c5770835cc01af2129b5c4e856109a1a22bf3b4
Patrick Golden <ptgolden0@gmail.com>
2015-04-11 15:17:55 -0400

    Improve patch diffing view

--- src/utils/period_diff.js
+++ src/utils/period_diff.js
@@ -6,10 +6,8 @@ var _ = require('underscore')
   , dmp = new DiffMatchPatch()
   , template = require('../templates/period.html')
 
-function d(period) {
-  var nodes = $(template({ period: period })).find('.field').toArray()
-    , ret = {}
-
+function periodToNodesObj(period) {
+  var nodes = $(template({ period: period })).find('.field').toArray();
   return Array.prototype.reduce.call(nodes, function (acc, el) {
     var key = el.children[0].textContent.trim();
     var val = el.children[1];
@@ -18,27 +16,68 @@ function d(period) {
   }, {});
 }
 
+// Compare two lists, and return which items were in the first; which were
+// in the second; and which were in both.
+function setDiff(fromarr, toarr) {
+  var ret = { both: [], from: [], to: [] }
+    , fromcmp = _.unique(fromarr).sort()
+    , tocmp = _.unique(toarr).sort()
+    , i = fromcmp.length - 1
+    , j = tocmp.length - 1
+
+  while (i >= 0) {
+    let item = fromcmp[i]
+      , found = false
+
+    for (; j >= 0; j--) {
+      if (tocmp[j] !== undefined && tocmp[j] === item) {
+        ret.both.push(tocmp.splice(j, 1)[0]);
+        found = true;
+        j--;
+        break;
+      }
+    }
+
+    if (!found) ret.from.push(item);
+    if (j === -1) j++;
+    i--;
+  }
+
+  ret.to = ret.to.concat(tocmp);
+
+  return ret;
+}
+
 module.exports = function (from, to) {
-  var fromNodes = d(from)
-    , toNodes = d(to)
+  var fromNodes = periodToNodesObj(from)
+    , toNodes = periodToNodesObj(to)
     , ret = '<dl class="dl-horizontal">'
 
   _.forEach(fromNodes, function (fromEl, key) {
     var toEl = toNodes[key];
 
     if (!toEl) {
-      ret += '<div class="diff-addition"><dt>' + key + '</dt><dd>' + toEl.innerHTML + '</dd></div>';
+      ret += '<div class="diff-deletion"><dt>' + key + '</dt><dd>' + toEl.innerHTML + '</dd></div>';
     } else if (fromEl.children.length) {
       let fromListItems = fromEl.children[0].children
-        , toListItems = fromEl.children[0].children
-        , fromListText = Array.prototype.map.call(fromListItems, el => el.textContent.trim()).join('\n')
-        , toListText = Array.prototype.map.call(toListItems, el => el.textContent.trim()).join('\n')
-        , diff = dmp.diff_main(fromListText, toListText)
-        , html = dmp.diff_prettyHtml(diff);
-        , html = html.split('\n').map(text => '<li>' + text + '</li>').join('\n')
+        , toListItems = toEl.children[0].children
+        , fromListText = Array.prototype.map.call(fromListItems, el => el.textContent.trim())
+        , toListText = Array.prototype.map.call(toListItems, el => el.textContent.trim())
+        , diffs = setDiff(fromListText, toListText)
 
-      ret += '<dt>' + key + '</dt><dd>' + html + '</dd>';
-
+      ret += '<dt>' + key + '</dt><dd><ul>';
+      ret += diffs.both.map(text => '<li>' + text + '</li>').join('');
+      if (diffs.from) {
+        ret += diffs.from
+          .map(text => '<li><span class="diff-deletion">' + text + '</span></li>')
+          .join('');
+      }
+      if (diffs.to) {
+        ret += diffs.to
+          .map(text => '<li><span class="diff-addition">' + text + '</span></li>')
+          .join('');
+      }
+      ret += '</ul></dd>';
     } else {
       let diff = dmp.diff_main(fromEl.textContent.trim(), toEl.textContent.trim());
       let html = dmp.diff_prettyHtml(diff);
@@ -49,7 +88,7 @@ module.exports = function (from, to) {
   });
 
   _.forEach(toNodes, function (toEl, key) {
-    ret += '<div class="diff-deletion"><dt>' + key + '</dt><dd>' + toEl.innerHTML + '</dd></div>';
+    ret += '<div class="diff-addition"><dt>' + key + '</dt><dd>' + toEl.innerHTML + '</dd></div>';
   });
 
   ret += '</dl>';
