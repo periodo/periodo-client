9324327d2987df9207ec82c2dad301307ef44110
Patrick Golden <ptgolden0@gmail.com>
2015-04-17 02:22:31 -0400

    When wrapping jquery.ajax promises, pass all arguments
    Non-jquery promises only take a single argument. When wrapping them, we
    now pass them in an array.

--- src/ajax.js
+++ src/ajax.js
@@ -5,13 +5,19 @@ module.exports = {
   ajax: function () {
     var args = arguments;
     return new Promise(function (resolve, reject) {
-      $.ajax.apply($, args).then(resolve, reject);
+      $.ajax.apply($, args).then(
+        ([...retArgs]) => resolve(retArgs),
+        ([...retArgs]) => reject(retArgs)
+      );
     });
   },
   getJSON: function () {
     var args = arguments;
     return new Promise(function (resolve, reject) {
-      $.getJSON.apply($, args).then(resolve, reject);
+      $.getJSON.apply($, args).then(
+        ([...retArgs]) => resolve(retArgs),
+        ([...retArgs]) => reject(retArgs)
+      );
     });
   }
 }
