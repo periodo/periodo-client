3c00397854bd036abe94edd86916851ab4627abb
Patrick Golden <ptgolden0@gmail.com>
2015-05-08 12:24:39 -0400

    Only throw error from date parser if explicitly specified to

--- src/utils/date_parser.js
+++ src/utils/date_parser.js
@@ -4,5 +4,16 @@ var fs = require('fs')
   , peg = require('pegjs')
   , grammar = fs.readFileSync(__dirname + '/date_parser.pegjs', 'utf8')
   , options = { allowedStartRules: ['start', 'gregorianyear', 'bpyear', 'iso8601year'] }
+  , parser = peg.buildParser(grammar, options)
 
-module.exports = peg.buildParser(grammar, options);
+module.exports = function (input, opts) {
+  opts = opts || {};
+
+  if (opts.throw) return parser.parse(input);
+
+  try {
+    return parser.parse(input);
+  } catch (e) {
+    return null;
+  }
+}
