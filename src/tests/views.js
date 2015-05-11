9974fe5ca62f0b16e9a14a137e4283a6c1bf7ce0
Patrick Golden <ptgolden0@gmail.com>
2015-05-10 21:20:38 -0400

    Update views to use new Immutable stuff

--- src/tests/views.js
+++ src/tests/views.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var assert = require('assert')
+  , Immutable = require('immutable')
 
 function appendDiv(container) {
   var el = document.createElement('div');
@@ -8,20 +9,16 @@ function appendDiv(container) {
   return el;
 }
 
-function copy(data) {
-  return JSON.parse(JSON.stringify(data));
-}
-
 describe('Period form', function () {
   var PeriodFormView = require('../views/period_edit')
-    , Period = require('../models/period')
     , container = document.createElement('div')
-    , period
-    , view
 
   function makePeriodFormView() {
-    var period = new Period({ label: 'Progressive era' })
-    return new PeriodFormView({ model: period, el: appendDiv(container) });
+    return new PeriodFormView({
+      model: Immutable.fromJS({ label: 'Progressive era' }),
+      store: Immutable.fromJS({}),
+      el: appendDiv(container)
+    });
   }
 
   before(function () { document.body.appendChild(container) });
@@ -29,15 +26,15 @@ describe('Period form', function () {
 
   it ('Should have bound the label automatically', function () {
     var view = makePeriodFormView();
-    assert.equal(view.$('[data-field="label"]').val(), 'Progressive era');
-    assert.equal(view.$('[data-field="label-language"]').text(), 'eng-latn');
+    assert.equal(view.$('[data-field="label"]').val().trim(), 'Progressive era');
+    assert.equal(view.$('[data-field="originalLabel"] [data-field="label-language"]').text().trim(), 'eng-latn');
   });
 
   it('Should parse dates for me', function () {
     var view = makePeriodFormView();
     view.$('#js-startLabel').val('1890').trigger('input');
     view.$('#js-endLabel').val('1920').trigger('input');
-    assert.deepEqual(view.model.toJSON(), {
+    assert.deepEqual(view.getData(), {
       label: 'Progressive era',
       originalLabel: {
         'eng-latn': 'Progressive era'
