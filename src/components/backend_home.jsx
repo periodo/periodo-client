d1c26244b1cc93f2950378a33a6075ce2c114d09
Patrick Golden <ptgolden0@gmail.com>
2015-06-22 17:31:21 -0400

    Add ability to browse backends by collection

--- src/components/backend_home.jsx
+++ src/components/backend_home.jsx
@@ -1,15 +1,105 @@
 "use strict";
 
 var React = require('react')
+  , CollectionList
+
+CollectionList = React.createClass({
+  displayName: 'CollectionList',
+  getInitialState: function () {
+    return { limit: 20, currentPage: 0 }
+  },
+  getMatchedCollections: function () {
+    var { describe } = require('../helpers/periodization')
+
+    return this.props.store.get('periodCollections')
+      .toSeq()
+      .skip(this.state.currentPage * this.state.limit)
+      .take(this.state.limit)
+      .map(describe)
+  },
+  render: function () {
+    var collections = this.getMatchedCollections()
+      , urlForCollection
+
+    urlForCollection = collectionID => (
+      this.props.router.generate('period-collection-show', {
+        collectionID: encodeURIComponent(collectionID),
+        backendName: this.props.backend.name
+      })
+    )
+
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Source title</th>
+            <th>Num. of periods</th>
+            <th>Earliest start</th>
+            <th>Latest stop</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            collections.map(collection =>
+              <tr>
+                <td>
+                  <a href={urlForCollection(collection.id)}>
+                    {collection.source}
+                  </a>
+                </td>
+                <td>{collection.definitions}</td>
+                <td>{collection.earliest}</td>
+                <td>{collection.latest}</td>
+              </tr>
+            )
+          }
+        </tbody>
+      </table>
+    )
+
+  }
+});
 
 module.exports = React.createClass({
   displayName: 'BackendHome',
+  getInitialState: function () {
+    return { browseBy: 'period' }
+  },
+  handleBrowseBy: function (browseBy) {
+    this.setState({ browseBy });
+  },
   render: function () {
     var FacetedBrowser = require('../views/faceted_browser/browser.jsx')
 
     return (
       <div>
-        <FacetedBrowser backend={this.props.backend} dataset={this.props.store} />
+        <div className="select-browse-item">
+          <h2>Browse by</h2>
+          <div>
+            <ul className="nav nav-pills">
+              <li className={this.state.browseBy === 'period' ? 'active' : ''}>
+                <a href="" onClick={this.handleBrowseBy.bind(null, 'period')}>
+                  Period
+                </a>
+              </li>
+              <li className={this.state.browseBy === 'collection' ? 'active' : ''}>
+                <a href="" onClick={this.handleBrowseBy.bind(null, 'collection')}>
+                  Collection
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className={this.state.browseBy === 'period' ? '' : 'hide'}>
+          <FacetedBrowser backend={this.props.backend} dataset={this.props.store} />
+        </div>
+        <div className={this.state.browseBy === 'period' ? 'hide' : ''}>
+          <CollectionList
+              backend={this.props.backend}
+              router={this.props.router}
+              store={this.props.store} />
+        </div>
       </div>
     )
   }
