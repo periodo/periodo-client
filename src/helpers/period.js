b52d136cc287e9ddfa993b7b0b5cfcfda27330a4
Patrick Golden <ptgolden0@gmail.com>
2015-07-01 12:10:37 -0400

    Add alternative period diffing function
    We still won't use it, but it might be the basis for a better
    implementation if the one in src/utils/period_diff.js doesn't work out
    in the end.

--- src/helpers/period.js
+++ src/helpers/period.js
@@ -1,7 +1,9 @@
 "use strict";
 
 var _ = require('underscore')
+  , React = require('react')
   , Immutable = require('immutable')
+  , diff = require('fast-diff')
 
 function getOriginalLabel(period) {
   var originalLabel = period.get('originalLabel')
@@ -48,4 +50,62 @@ function validate(period) {
   return _.isEmpty(errors) ? null : errors;
 }
 
-module.exports = { validate, getOriginalLabel, getAlternateLabels }
+function wrapTextLeaves(className, item) {
+  debugger;
+  return item instanceof Immutable.Iterable ?
+    item.map(wrapTextLeaves.bind(null, className)) :
+    <span className={className}>{item}</span>
+}
+
+function diffTree(from, to) {
+  if (Immutable.is(from, to)) return from;
+
+  if (to && from === undefined) return wrapTextLeaves('diff-addition', to);
+
+  if (to === undefined && from) return wrapTextLeaves('diff-deletion', from);
+
+  if (from instanceof Immutable.Map) {
+    let diffed = Immutable.Map()
+      , processEntry
+
+    processEntry = (fromVal, toVal, key) => {
+      if (toVal instanceof Immutable.List) {
+        toVal = toVal.toSet();
+        fromVal = fromVal.toSet();
+      }
+
+      diffed = diffed.set(key, diffTree(fromVal, toVal));
+    }
+
+    to.forEach((toVal, key) => processEntry(from.get(key), toVal, key));
+    from.filter((val, key) => !to.has(key))
+      .forEach((fromVal, key) => processEntry(fromVal, undefined, key))
+
+    return diffed;
+  }
+
+  if (from instanceof Immutable.Set) {
+    let diffed = Immutable.List()
+
+    diffed = diffed.concat(from.subtract(to).map(wrapTextLeaves.bind(null, 'diff-deletion')));
+    diffed = diffed.concat(to.subtract(from).map(wrapTextLeaves.bind(null, 'diff-addition')));
+    diffed = diffed.concat(to.intersect(from));
+
+    return diffed
+  }
+
+  if (typeof from === 'string') {
+    return <span>{diff(from, to).map(diffToNode)}</span>;
+  }
+}
+
+function diffToNode([type, text]) {
+  var className = ''
+
+  if (type === diff.INSERT) className = 'diff-addition';
+  if (type === diff.DELETE) className = 'diff-deletion';
+
+  return <span key={'k' + Math.random()} className={className}>{text}</span>
+}
+
+module.exports = { validate, getOriginalLabel, getAlternateLabels, diffTree }
