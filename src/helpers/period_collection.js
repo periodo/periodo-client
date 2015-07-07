0c48f8b6d49b30475eab019ab9ae7c8b83ecfb2b
Patrick Golden <ptgolden0@gmail.com>
2015-07-07 18:45:10 -0400

    Fix binning algorithm; allow explicity bin min and max

--- src/helpers/period_collection.js
+++ src/helpers/period_collection.js
@@ -2,13 +2,15 @@
 
 var Immutable = require('immutable')
 
-function makeRangeBins(periods, numBins) {
+function makeRangeBins(periods, numBins, min, max) {
   var { getEarliestYear, getLatestYear } = require('./terminus')
-    , min = Infinity
-    , max = -Infinity
+    , setMin = Infinity
+    , setMax = -Infinity
     , termini
     , bins
 
+  // First, get a list of all termini which have ISO values, while
+  // simultaneously keeping track of the maximum and minimum
   termini = Immutable.List().withMutations(list => {
     let iter = periods.values()
       , period
@@ -17,18 +19,29 @@ function makeRangeBins(periods, numBins) {
       let earliest = getEarliestYear(period.get('start'))
         , latest = getLatestYear(period.get('stop'))
 
-      if (earliest && latest) {
+      if (earliest !== null && latest !== null) {
         list.push(Immutable.Map({ earliest, latest }));
-        if (earliest < min) min = earliest;
-        if (latest > max) max = latest;
+        if (earliest < setMin) setMin = earliest;
+        if (latest > setMax) setMax = latest;
       }
     }
   });
 
+  if (min === undefined || max === undefined) {
+    min = setMin;
+    max = setMax;
+  }
+
+  // termini = termini.sortBy(terminus => terminus.get('start'));
+
+  // If there were no periods with ISO years, throw an error.
+  //
+  // FIXME: maybe return null instead
   if (!isFinite(min) || !isFinite(max)) {
     throw new Error('Could not find minimum and maximum for periods.')
   }
 
+  // Now, create a series of bins that will contain all of the given values
   bins = Immutable.Range(min, max, (min - max) / numBins).map((binStart, i, all) => {
     let binStop = all.get(i + 1) || max;
     return Immutable.Map({ earliest: binStart, latest: binStop, count: 0 })
@@ -36,19 +49,24 @@ function makeRangeBins(periods, numBins) {
 
   return bins.toList().withMutations(function (_bins) {
     termini.forEach(terminus => {
+      var termMin = terminus.get('earliest')
+        , termMax = terminus.get('latest')
+
       _bins.forEach((bin, i) => {
-        if (bin.get('earliest') < terminus.get('earliest')) {
-          // terminus not in range yet, continue iteration
-          return true;
-        } else if (bin.get('earliest') >= terminus.get('latest')) {
-          // terminus out of range, stop iteration
-          return false;
-        } else {
-          // terminus in range; add to count
-          _bins.updateIn([i, 'count'], c => c + 1);
-        }
+        let binMax = bin.get('latest')
+
+        // If the terminus is not in a bin yet, continue iteration
+        if (termMin >= binMax) return true;
+
+        // The terminus is in a bin, so update its count.
+        _bins.updateIn([i, 'count'], c => c + 1);
+
+        // If we have added one to the count of every bin that this period
+        // spans, we can stop iterating.
+        if (termMax <= binMax) return false;
       });
     });
+    _bins._range = [setMin, setMax];
   });
 }
 
--- src/tests/helpers.js
+++ src/tests/helpers.js
@@ -181,6 +181,24 @@ describe('Period collection helpers', function () {
       { earliest: 175, latest: 200, count: 3 }
     ]);
 
+    var periods2 = Immutable.fromJS([
+      { start: { in: { year: -10000 }}, stop: { in: { year: 0 }}},
+      { start: { in: { year: -12 }}, stop: { in: { year: -1 }}}
+    ]);
+
+    assert.deepEqual(makeRangeBins(periods2, 10).toJS(), [
+      { earliest: -10000, latest: -9000, count: 1 },
+      { earliest: -9000, latest: -8000, count: 1 },
+      { earliest: -8000, latest: -7000, count: 1 },
+      { earliest: -7000, latest: -6000, count: 1 },
+      { earliest: -6000, latest: -5000, count: 1 },
+      { earliest: -5000, latest: -4000, count: 1 },
+      { earliest: -4000, latest: -3000, count: 1 },
+      { earliest: -3000, latest: -2000, count: 1 },
+      { earliest: -2000, latest: -1000, count: 1 },
+      { earliest: -1000, latest: 0, count: 2 }
+    ]);
+
   });
 });
 
