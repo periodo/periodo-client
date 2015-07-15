9f1c26ed40e6fa37586550c5cce3410a147fabb8
Patrick Golden <ptgolden0@gmail.com>
2015-07-15 16:04:05 -0400

    Allow skolem ID generation id to take protocol & host parameters
    this allows it to be used outside of a browser

--- src/utils/generate_skolem_id.js
+++ src/utils/generate_skolem_id.js
@@ -2,17 +2,16 @@
 
 var url = require('url')
 
-module.exports = function (length) {
+module.exports = function (protocol=location.protocol, host=location.host, length=32) {
   var hexstr = '';
 
-  length = length || 32;
   for (var i = 0; i < length; i++) {
     hexstr += Math.floor(Math.random() * 16).toString(16);
   }
 
   return url.format({
-    protocol: location.protocol,
-    host: location.host,
+    protocol,
+    host,
     pathname: '/.well-known/genid/' + hexstr
   });
 }
