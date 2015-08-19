19aa9353e1a592e21ea383d46afe0e9fb5352de9
Patrick Golden <ptgolden0@gmail.com>
2015-08-19 15:43:23 -0400

    Fix the typo from when I fixed the typo

--- src/router.js
+++ src/router.js
@@ -137,11 +137,11 @@ module.exports = React.createClass({
 
     if (path.indexOf('/') !== -1) this.showNotFound();
 
-    getBackendAndStore('canonical')
+    getBackendAndStore('Canonical')
       .then(({ store }) => {
         if (store.hasIn(['periodCollections', matchKey])) {
           let redirectURL = this.state.router.generate('period-collection-show', {
-            backendName: 'canonical',
+            backendName: 'Canonical',
             collectionID: encodeURIComponent(matchKey)
           })
 
@@ -160,7 +160,7 @@ module.exports = React.createClass({
 
           if (collectionID) {
             let redirectURL = this.state.router.generate('period-collection-show', {
-              backendName: 'canonical',
+              backendName: 'Canonical',
               collectionID
             });
 
