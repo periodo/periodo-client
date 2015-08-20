55965dc7ab5fff61fd2d80d5ffd4e1805cf4aa11
Patrick Golden <ptgolden0@gmail.com>
2015-08-20 17:18:32 -0400

    Add prefixes to datatypes in turtle writer, when possible

--- src/utils/jsonld_to_turtle.js
+++ src/utils/jsonld_to_turtle.js
@@ -3,6 +3,31 @@
 var jsonld = require('jsonld')
   , N3 = require('n3')
 
+const PREFIXES = {
+  skos: 'http://www.w3.org/2004/02/skos/core#',
+  dcterms: 'http://purl.org/dc/terms/',
+  foaf: 'http://xmlns.com/foaf/0.1/',
+  time: 'http://www.w3.org/2006/time#',
+  xsd: 'http://www.w3.org/2001/XMLSchema#',
+  owl: 'http://www.w3.org/2002/07/owl#',
+  bibo: 'http://purl.org/ontology/bibo/',
+  periodo: 'http://n2t.net/ark:/99152/p0v#'
+}
+
+function addPrefix(uri) {
+  for (var prefix in PREFIXES) {
+    let prefixedURI = PREFIXES[prefix]
+
+    if (!PREFIXES.hasOwnProperty(prefix)) continue;
+
+    if (uri.indexOf(prefixedURI) === 0) {
+      return `${prefix}:${uri.replace(prefixedURI, '')}`
+    }
+  }
+
+  return uri;
+}
+
 function processStatement(statement) {
   var val;
 
@@ -14,7 +39,7 @@ function processStatement(statement) {
   } else if (statement.datatype === 'http://www.w3.org/1999/02/22-rdf-syntax-ns#langString') {
     val += '@' + statement.language;
   } else {
-    val += '^^' + statement.datatype;
+    val += '^^' + addPrefix(statement.datatype);
   }
 
   return val;
@@ -27,16 +52,7 @@ module.exports = function (jsonldData) {
 
       if (err) reject(err);
 
-      writer = N3.Writer({
-        skos: 'http://www.w3.org/2004/02/skos/core#',
-        dcterms: 'http://purl.org/dc/terms/',
-        foaf: 'http://xmlns.com/foaf/0.1/',
-        time: 'http://www.w3.org/2006/time#',
-        xsd: 'http://www.w3.org/2001/XMLSchema#',
-        owl: 'http://www.w3.org/2002/07/owl#',
-        bibo: 'http://purl.org/ontology/bibo/',
-        periodo: 'http://n2t.net/ark:/99152/p0v#'
-      });
+      writer = N3.Writer(PREFIXES);
 
       dataset['@default'].forEach(triple => writer.addTriple({
         subject: processStatement(triple.subject),
