f21695eb1d30edd145f6570046f055e9abfddd3c
Patrick Golden <ptgolden0@gmail.com>
2015-12-10 19:07:12 -0500

    Various fixes in release script
    all cards on the table: I wrote that code almost a month ago and I
    don't particularly remember where I left off. I'm pretty sure it was
    finished. I just finished my last paper of the semester, though, and I
    want to get on with more interesting things.

--- bin/release.js
+++ bin/release.js
@@ -13,13 +13,16 @@ const BASE_URL = 'https://api.github.com/repos/periodo/periodo-client'
     , ZIPFILE_NAME = `periodo-${VERSION}.zip`
     , VERSION_ZIPFILE = path.join(__dirname, '..', 'dist', ZIPFILE_NAME)
     , TAG = 'v' + VERSION
-    , TOKEN = fs.readFileSync(`${process.env.HOME}/.githubtoken`)
+    , TOKEN = fs.readFileSync(`${process.env.HOME}/.githubtoken`, { encoding: 'utf-8' }).trim()
 
 
 Promise.all([tagExists(), releaseDoesNotExist(), releaseZipExists()])
-  .then(() => console.log('OK'))
   .then(postRelease)
+  .then(getAssetURL)
   .then(postZip)
+  .then(() => {
+    console.log('Release uploaded.');
+  })
   .catch(err => {
     process.stderr.write('Could not release version. Details:\n\n');
     process.stderr.write(err.stack || err);
@@ -30,6 +33,8 @@ Promise.all([tagExists(), releaseDoesNotExist(), releaseZipExists()])
 function postRelease() {
   var opts = url.parse(`${BASE_URL}/releases`)
     , data = JSON.stringify({ 'tag_name': TAG, 'name': TAG })
+
+  console.log('Posting new release...');
 
   opts.method = 'POST';
   opts.headers = {
@@ -42,37 +47,75 @@ function postRelease() {
   return new Promise((resolve, reject) => {
     var req = https.request(opts, res => {
       if (res.statusCode === 201) {
-        resolve(res.headers.Location)
+        resolve(res.headers.location)
       } else {
         reject('Bad status code: ' + res.statusCode);
       }
     });
 
     req.on('error', reject);
+
+    req.write(data);
     req.end();
   });
 }
 
-function postZip(releaseURL) {
-  var opts = url.parse(releaseURL + `/assets?name=zipFilename`)
+
+function getAssetURL(releaseURL) {
+  var opts = url.parse(releaseURL)
+
+  opts.headers = {
+    'Authorization': `token ${TOKEN}`,
+    'User-Agent': 'node.js'
+  }
+
+  return new Promise((resolve, reject) => {
+    https.get(opts, res => {
+      var body = '';
+
+      if (res.statusCode !== 200) {
+        reject(`Problem fetching release (${res.statusCode})`);
+      }
+
+      res.setEncoding('utf-8');
+      res.on('data', d => body += d);
+      res.on('end', () => {
+        body = JSON.parse(body);
+        resolve(body.upload_url)
+      })
+
+    }).on('error', reject);
+  });
+}
+
+
+function postZip(assetURL) {
+  var opts = url.parse(assetURL.replace('{?name,label}', `?name=${ZIPFILE_NAME}`))
 
   opts.method = 'POST';
   opts.headers = {
     'Authorization': `token ${TOKEN}`,
     'Content-Type': 'application/zip',
+    'Content-Length': fs.statSync(VERSION_ZIPFILE).size,
     'User-Agent': 'node.js'
   }
 
-  return new Promise((resolve, reject) => {
-    var req = https.request(opts, res => {
-      if (res.statusCode === 201) resolve();
-      else reject('Failed to upload zip.');
-    });
+  console.log('Uploading zipped release...');
 
-    fs.createReadStream(VERSION_ZIPFILE).pipe(req);
-    req.end();
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      var req = https.request(opts, res => {
+        if (res.statusCode === 201) resolve();
+        else reject('Failed to upload zip.');
+      });
+
+      req.on('error', reject);
+
+      fs.createReadStream(VERSION_ZIPFILE).pipe(req);
+    }, 250);
   });
 }
+
 
 function releaseZipExists() {
   return new Promise((resolve, reject) => {
@@ -85,10 +128,12 @@ function releaseZipExists() {
   });
 }
 
+
 function releaseDoesNotExist() {
   var opts = url.parse(`${BASE_URL}/releases/tags/${TAG}`)
 
   opts.headers = {
+    'Authorization': `token ${TOKEN}`,
     'User-Agent': 'node.js'
   }
 
@@ -101,10 +146,12 @@ function releaseDoesNotExist() {
   });
 }
 
+
 function tagExists() {
   var opts = url.parse(`${BASE_URL}/git/refs/tags/${TAG}`)
 
   opts.headers = {
+    'Authorization': `token ${TOKEN}`,
     'User-Agent': 'node.js'
   }
 
