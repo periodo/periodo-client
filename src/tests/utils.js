6f7a9d07435eb570074705213373a8705f162025
Patrick Golden <ptgolden0@gmail.com>
2016-05-31 14:15:33 -0400

    Move utils tests to use tape

--- src/tests/utils.js
+++ src/tests/utils.js
@@ -1,198 +1,163 @@
-/* global describe, it */
-
 "use strict";
 
-var assert = require('assert')
-  , Immutable = require('immutable')
-
-describe('Date parser', function () {
-  var parse = require('../utils/date_parser')
-
-  it('should parse valid ISO years', function () {
-    assert.deepEqual(parse('1994'), {
-      _type: 'iso8601',
-      label: '1994',
-      'in': { year: '1994' }
-    });
-
-    assert.deepEqual(parse('-50000'), {
-      _type: 'iso8601',
-      label: '-50000',
-      'in': { year: '-50000' }
-    });
-
-    // ISO8601 years without a +/- must be 4 digits.
-    /*
-    assert.throws(isoParser('4'), parser.SyntaxError);
-    assert.throws(isoParser('500000000'), parser.SyntaxError);
-    */
-
-    // People didn't like the above constraint, so those are actually OK.
-    assert.deepEqual(parse('0043'), {
-      _type: 'iso8601',
-      label: '0043',
-      'in': { year: '0043' }
-    });
-  });
-
-  it('should parse valid Gregorian years', function () {
-    function gregorianParser(val) {
-      return parse(val, { startRule: 'gregorianyear', throw: true });
-    }
-
-    assert.deepEqual(parse('12 A.D.'), {
-      _type: 'gregorian',
-      label: '12 A.D.',
-      'in': { year: '0012' }
-    });
-
-    assert.deepEqual(parse('12 bc'), {
-      _type: 'gregorian',
-      label: '12 bc',
-      'in': { year: '-0011' }
-    });
-
-    assert.deepEqual(parse('12 BCE'), {
-      _type: 'gregorian',
-      label: '12 BCE',
-      'in': { year: '-0011' }
-    });
-
-    assert.deepEqual(parse('12 bc').value, parse('12 B.C.').value);
-    assert.deepEqual(parse('12 bc').value, parse('12 b.c.e.').value);
-
-    // Gregorian years must have BC or AD
-    assert.throws(gregorianParser.bind(null, '2014'), parse._parser.SyntaxError);
-  });
-
-  it('should parse valid BP years', function () {
-    function bpParse(val, base) {
-      return parse(val, { startRule: 'bpyear', bpBase: base, throw: true  });
-    }
-
-    assert.deepEqual(parse('5 b.p.'), {
-      _type: 'bp1950',
-      label: '5 b.p.',
-      'in': { year: '1945' }
-    });
-
-    assert.deepEqual(bpParse('2000 BP', 2000), {
-      _type: 'bp2000',
-      label: '2000 BP',
-      'in': { year: '0000' }
-    });
-
-    assert.deepEqual(parse('5 BP2000'), {
-      _type: 'bp2000',
-      label: '5 BP2000',
-      'in': { year: '1995' }
-    });
-  });
-  
-  it('should handle approximate dates', function () {
-    assert.deepEqual(parse('c. 5000 BCE'), {
-      _type: 'gregorian',
-      label: 'c. 5000 BCE',
-      'in': { year: '-4999' },
-    });
-
-    assert.deepEqual(parse('~800'), {
-      _type: 'iso8601',
-      label: '~800',
-      'in': { year: '0800' },
-    });
-
-    assert.deepEqual(parse('Ca. 12 AD'), {
-      _type: 'gregorian',
-      label: 'Ca. 12 AD',
-      'in': { year: '0012' },
-    });
-
-    assert.deepEqual(parse('circa 1000 BC'), {
-      _type: 'gregorian',
-      label: 'circa 1000 BC',
-      'in': { year: '-0999' },
-    });
-
-    assert.deepEqual(parse('3200? BC'), {
-      _type: 'gregorian',
-      label: '3200? BC',
-      'in': { year: '-3199' },
-    });
-  });
-
-  it('should handle descriptions of centuries or millenia', function () {
-    assert.deepEqual(parse('21st century'), {
-      _type: 'gregorian',
-      label: '21st century',
-      'in': { earliestYear: '2001', latestYear: '2100' }
-    });
-
-    assert.deepEqual(parse('21st century BC'), {
-      _type: 'gregorian',
-      label: '21st century BC',
-      'in': { earliestYear: '-2099', latestYear: '-2000' }
-    });
-
-    assert.deepEqual(parse('early 21st century'), {
-      _type: 'gregorian',
-      label: 'early 21st century',
-      'in': { earliestYear: '2001', latestYear: '2034' }
-    });
-
-    assert.deepEqual(parse('middle of the third century'), {
-      _type: 'gregorian',
-      label: 'middle of the third century',
-      'in': { earliestYear: '0234', latestYear: '0267' }
-    });
-
-    assert.deepEqual(parse('First century BC'), {
-      _type: 'gregorian',
-      label: 'First century BC',
-      'in': { earliestYear: '-0099', latestYear: '0000'}
-    });
-  });
-
-  it('should handle date ranges', function () {
-    assert.deepEqual(parse('410/314 BCE'), {
-      _type: 'gregorian',
-      label: '410/314 BCE',
-      'in': { earliestYear: '-0409', latestYear: '-0313' }
-    });
-
-    assert.deepEqual(parse('1200/1400'), {
-      _type: 'iso8601',
-      label: '1200/1400',
-      'in': { earliestYear: '1200', latestYear: '1400' }
-    });
-  });
-
-  it('should accept \'present\'', function () {
-    assert.deepEqual(parse('present'), {
-      _type: 'present',
-      label: 'present'
-    });
-  });
+const test = require('tape')
+    , Immutable = require('immutable')
 
 
-  it('should throw errors for invalid dates only when asked', function () {
-    // No support for months (yet)
-    assert.throws(parse.bind(null, 'December 1945', { throw: true }), parse._parser.SyntaxError);
-    assert.equal(parse('December 1945'), null);
+const parse = require('../utils/date_parser')
 
-    // No support for gibberish (yet)
-    assert.throws(parse.bind(null, 'afeopoij', { throw: true }), parse._parser.SyntaxError);
-    assert.equal(parse('afeopoij'), null);
-  });
+const formatYear = (format, label, yearOrYears) => ({
+  _type: format,
+  label,
+  'in': Array.isArray(yearOrYears)
+    ? { earliestYear: yearOrYears[0], latestYear: yearOrYears[1] }
+    : { year: yearOrYears }
+})
 
+
+test('Date parser (single dates)', t => {
+  t.plan(11)
+
+  t.comment('## ISO8601')
+
+  t.deepEqual([
+    parse('1994'),
+    parse('-50000'),
+    parse('0043'),
+  ], [
+    formatYear('iso8601', '1994', '1994'),
+    formatYear('iso8601', '-50000','-50000'),
+    formatYear('iso8601', '0043', '0043'),
+  ], 'should parse valid ISO years')
+
+
+  /*
+  // ISO8601 years without a +/- must be 4 digits.
+  // (People didn't like the above constraint, so those are actually OK.)
+  assert.throws(isoParser('4'), parser.SyntaxError);
+  assert.throws(isoParser('500000000'), parser.SyntaxError);
+  */
+
+  t.comment('## Gregorian')
+
+  t.deepEqual([
+    parse('12 A.D.'),
+    parse('12 bc'),
+    parse('12 BCE'),
+  ], [
+    formatYear('gregorian', '12 A.D.', '0012'),
+    formatYear('gregorian', '12 bc', '-0011'),
+    formatYear('gregorian', '12 BCE', '-0011'),
+  ], 'should parse valid Gregorian years')
+
+  t.deepEqual([
+    parse('12 bc').value,
+    parse('12 bc').value
+  ], [
+    parse('12 B.C.').value,
+    parse('12 b.c.e.').value,
+  ], 'should normalize AD/BC(E) labels')
+
+  t.throws(
+    () => parse('2014', { startRule: 'gregorianyear', throw: true  }),
+    parse._parser.SyntaxError,
+    'should throw if Gregorian parse is not passed a suffix'
+  );
+
+
+  t.comment('## BP')
+
+  t.deepEqual(
+    parse('5 b.p.'),
+    formatYear('bp1950', '5 b.p.', '1945'),
+    'should parse valid BP years, assuming base of 1950.'
+  );
+
+  t.deepEqual(
+    parse('2000 BP', { bpBase: 2000 }),
+    formatYear('bp2000', '2000 BP', '0000'),
+    'should allow BP base to be configured in a parser option.'
+  )
+
+  t.deepEqual(
+    parse('5 BP2000'),
+    formatYear('bp2000', '5 BP2000', '1995'),
+    'should intuit BP base if provided in the parsed string'
+  );
+
+
+  t.comment('## Approximate dates')
+
+  t.deepEqual([
+    parse('c. 5000 BCE'),
+    parse('~800'),
+    parse('Ca. 12 AD'),
+    parse('circa 1000 BC'),
+    parse('3200? BC'),
+  ], [
+    formatYear('gregorian', 'c. 5000 BCE', '-4999'),
+    formatYear('iso8601', '~800', '0800'),
+    formatYear('gregorian', 'Ca. 12 AD', '0012'),
+    formatYear('gregorian', 'circa 1000 BC', '-0999'),
+    formatYear('gregorian', '3200? BC', '-3199'),
+  ], 'should handle approximate dates');
+
+  t.deepEqual(
+    parse('present'),
+    { _type: 'present', label: 'present' },
+    "should accept 'present' as a valid date"
+  );
+
+  t.throws(
+    () => parse('asdlfkj', { throw: true }),
+    parse._parser.SyntaxError,
+    'should throw errors for invalid dates when specified'
+  );
+
+  t.equal(
+    parse('asdlfkj'),
+    null,
+    'should return null for invalid dates by default'
+  )
 });
 
-describe('Patch utils', function () {
-  var { makePatch } = require('../utils/patch')
-    , data = Immutable.fromJS(require('./data/period-collection.json'))
-    , samplePatches
 
-  samplePatches = {
+test('Date parser (ranges)', t => {
+  t.plan(2);
+
+  t.deepEqual([
+    parse('21st century'),
+    parse('21st century BC'),
+    parse('early 21st century'),
+    parse('middle of the third century'),
+    parse('First century BC'),
+  ], [
+    formatYear('gregorian', '21st century', ['2001', '2100']),
+    formatYear('gregorian', '21st century BC', ['-2099', '-2000']),
+    formatYear('gregorian', 'early 21st century', ['2001', '2034']),
+    formatYear('gregorian', 'middle of the third century', ['0234', '0267']),
+    formatYear('gregorian', 'First century BC', ['-0099', '0000']),
+  ], 'should handle descriptions of centuries or millenia');
+
+  t.deepEqual([
+    parse('410/314 BCE'),
+    parse('1200/1400'),
+  ], [
+    formatYear('gregorian', '410/314 BCE', ['-0409', '-0313']),
+    formatYear('iso8601', '1200/1400', ['1200', '1400']),
+  ], 'should handle "slashed" date ranges');
+
+})
+
+
+test('Patch utils', t => {
+
+  t.plan(7);
+
+  const { makePatch } = require('../utils/patch')
+      , data = Immutable.fromJS(require('./data/period-collection.json'))
+
+  const samplePatches = {
     addPeriod: {
       op: 'add',
       path: '/periodCollections/a/definitions/b'
@@ -207,109 +172,112 @@ describe('Patch utils', function () {
     }
   }
 
-  it('should create an empty array between two identical datasets', function () {
-    assert.deepEqual(makePatch(data.toJS(), data.toJS()), []);
-  });
+  t.deepEqual(
+    makePatch(data.toJS(), data.toJS()),
+    [],
+    'should not detect any changes between two identical datasets'
+  );
 
-  it('should group patches together', function () {
-    var { groupByChangeType } = require('../helpers/patch_collection')
+
+  const { groupByChangeType } = require('../helpers/patch_collection')
       , patches = Immutable.fromJS(samplePatches).toList()
 
-    assert.deepEqual(groupByChangeType(patches).toJS(), {
-      addPeriod: {
-        a: [samplePatches.addPeriod]
-      },
-      removePeriod: {
-        a: [samplePatches.removePeriod]
-      },
-      editPeriod: {
-        a: {
-          b: [samplePatches.changePeriod]
-        }
+  t.deepEqual(groupByChangeType(patches).toJS(), {
+    addPeriod: {
+      a: [samplePatches.addPeriod]
+    },
+    removePeriod: {
+      a: [samplePatches.removePeriod]
+    },
+    editPeriod: {
+      a: {
+        b: [samplePatches.changePeriod]
       }
-    });
-  });
+    }
+  }, 'should group patches together');
 
-  it('should use "add" operation for simple values instead of "replace"', function () {
-    var attrPath = ['periodCollections', 'p03377f', 'source']
+
+  const attrPath = ['periodCollections', 'p03377f', 'source']
       , newData = data.setIn(attrPath.concat('yearPublished'), '1900')
       , patch = makePatch(data.toJS(), newData.toJS())
 
-    assert.deepEqual(patch, [
-      {
-        op: 'add',
-        path: '/' + attrPath.join('/'),
-        value: newData.getIn(attrPath).toJS()
-      }
-    ]);
+  t.deepEqual(patch, [
+    {
+      op: 'add',
+      path: '/' + attrPath.join('/'),
+      value: newData.getIn(attrPath).toJS()
+    }
+  ], 'should use "add" operation for simple values instead of "replace"');
+
+
+  const attrPath2 = ['periodCollections', 'p03377f', 'definitions', 'p03377fkhrv', 'spatialCoverage']
+
+  const newData2 = data.updateIn(attrPath2, sc => {
+    return sc.unshift(Immutable.Map({ id: 'http://example.com/', label: 'New country' }));
   });
 
-  it('should use "add" operation for complex values instead of "replace"', function () {
-    var attrPath = ['periodCollections', 'p03377f', 'definitions', 'p03377fkhrv', 'spatialCoverage']
-      , newData
-      , patch
+  const patch2 = makePatch(data.toJS(), newData2.toJS());
 
-    newData = data.updateIn(attrPath, (function (sc) {
-      return sc.unshift(Immutable.Map({ id: 'http://example.com/', label: 'New country' }));
-    }));
+  t.deepEqual(patch2, [
+    {
+      op: 'add',
+      path: '/' + attrPath2.join('/'),
+      value: newData2.getIn(attrPath2).toJS()
+    }
+  ], 'should use "add" operation for complex values instead of "replace"');
 
-    patch = makePatch(data.toJS(), newData.toJS());
 
-    assert.deepEqual(patch, [
-      {
-        op: 'add',
-        path: '/' + attrPath.join('/'),
-        value: newData.getIn(attrPath).toJS()
-      }
-    ]);
-  });
+  const { classifyPatch, parsePatchPath, patchTypes } = require('../utils/patch')
 
-  it('should classify patch paths', function () {
-    var { classifyPatch, parsePatchPath, patchTypes } = require('../utils/patch')
+  t.deepEqual([
+    classifyPatch(samplePatches.addPeriod),
+    classifyPatch(samplePatches.removePeriod),
+    classifyPatch(samplePatches.changePeriod),
+  ], [
+    [patchTypes.CREATE_PERIOD, 'Created period b in collection a.'],
+    [patchTypes.DELETE_PERIOD, 'Deleted period b in collection a.'],
+    [patchTypes.EDIT_PERIOD, 'Changed note of period b in collection a.'],
+  ], 'should classify patch paths');
 
-    assert.deepEqual(classifyPatch(samplePatches.addPeriod),
-      [patchTypes.CREATE_PERIOD, 'Created period b in collection a.']);
-    assert.deepEqual(classifyPatch(samplePatches.removePeriod),
-      [patchTypes.DELETE_PERIOD, 'Deleted period b in collection a.']);
-    assert.deepEqual(classifyPatch(samplePatches.changePeriod),
-      [patchTypes.EDIT_PERIOD, 'Changed note of period b in collection a.']);
 
-    assert.deepEqual(parsePatchPath(samplePatches.addPeriod), {
+  t.deepEqual([
+    parsePatchPath(samplePatches.addPeriod),
+    parsePatchPath(samplePatches.removePeriod),
+    parsePatchPath(samplePatches.changePeriod),
+    parsePatchPath({ path: '/periodCollections/abc/definitions/def/spatialCoverageDescription' }),
+  ], [
+    {
       label: null,
       type: 'period',
       collection_id: 'a',
       id: 'b',
-    });
-
-    assert.deepEqual(parsePatchPath(samplePatches.removePeriod), {
+    },
+    {
       label: null,
       type: 'period',
       collection_id: 'a',
       id: 'b',
-    });
-
-    assert.deepEqual(parsePatchPath(samplePatches.changePeriod), {
+    },
+    {
       label: 'note',
       type: 'period',
       collection_id: 'a',
       id: 'b'
-    });
+    },
+    {
+      label: 'spatialCoverageDescription',
+      type: 'period',
+      collection_id: 'abc',
+      id: 'def'
+    },
+  ], 'should parse information from patch paths');
 
-    assert.deepEqual(
-      parsePatchPath({
-        path: '/periodCollections/abc/definitions/def/spatialCoverageDescription'
-      }),
-      {
-        label: 'spatialCoverageDescription',
-        type: 'period',
-        collection_id: 'abc',
-        id: 'def'
-      }
-    );
-
-    assert.throws(parsePatchPath.bind({
+  t.throws(
+    () => parsePatchPath({
       op: 'add',
       path: '/periodCollections/a/definitions/b/madeUpField'
-    }))
-  });
+    }),
+    new Error('Invalid field for a period: madeUpField'),
+    'Should throw when attempting to parse invalid patch path.'
+  );
 });
