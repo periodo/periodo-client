a6be3f7cf90d28cba64221eb080ef1c21a5f0839
Patrick Golden <ptgolden0@gmail.com>
2016-06-13 12:34:35 -0400

    Pass db as an extra argument using redux-thunk
    This will both simplify action creator code and allow us to easily mock
    indexedDB in non-browser or non-compliant environments

--- src/index.js
+++ src/index.js
@@ -3,17 +3,18 @@
 const h = require('react-hyperscript')
     , ReactDOM = require('react-dom')
     , fastclick = require('fastclick')
-    , { createStore, applyMiddleware, compose } = require('redux')
     , thunk = require('redux-thunk').default
-    , rootReducer = require('./reducers')
+    , { createStore, applyMiddleware, compose } = require('redux')
     , Root = require('./components/root')
+    , rootReducer = require('./reducers')
+    , periodoDB = require('./db')
 
 
 function initialize() {
   const store = createStore(
     rootReducer,
     compose(
-      applyMiddleware(thunk),
+      applyMiddleware(thunk.withExtraArgument({ db: periodoDB() })),
       window.devToolsExtension ? window.devToolsExtension() : undefined
     )
   )
