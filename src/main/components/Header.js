d60e59a196244c0897c52f10b57869c23621ed67
Patrick Golden <ptgolden0@gmail.com>
2017-05-19 18:52:20 -0400

    Add 'select backend' to header

--- src/main/components/Header.js
+++ src/main/components/Header.js
@@ -1,12 +1,17 @@
 "use strict";
 
 const h = require('react-hyperscript')
+    , RouterKnower = require('../../shared/components/router_knower')
 
-const Header = () =>
+const Header = props =>
   h('div .flex', [
     h('h1 .m0 .blue .regular .h2', [
-      h('a .blue .text-decoration-none', { href: '/#/' }, 'PeriodO')
-    ])
+      h('a .blue .text-decoration-none', { href: '/#/' }, 'PeriodO'),
+    ]),
+
+    h('a .ml1 .blue .text-decoration-none', {
+      href: props.generateRoute('backend-select')
+    }, '[select backend]')
   ])
 
-module.exports = Header;
+module.exports = RouterKnower(Header);
