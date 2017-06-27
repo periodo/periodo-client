67873336948afaa8e986cf102d85f5f5d98fc6bc
Patrick Golden <ptgolden0@gmail.com>
2017-06-27 14:04:04 -0400

    Remove extraneous breadcrumbs

--- src/backends/components/Authority.js
+++ src/backends/components/Authority.js
@@ -3,9 +3,7 @@
 const h = require('react-hyperscript')
     , React = require('react')
     , { Box } = require('axs-ui')
-    , { Source, Breadcrumb, Link } = require('lib/ui')
-    , { Route } = require('lib/router')
-    , PeriodForm = require('../../editors/PeriodForm')
+    , { Source } = require('lib/ui')
 
 module.exports = class Authority extends React.Component {
   constructor() {
@@ -19,24 +17,11 @@ module.exports = class Authority extends React.Component {
 
   render() {
     const { backend, id } = this.props
-        , { addingPeriod, period } = this.state
         , authority = backend.dataset.periodCollections[id]
 
     return (
       h(Box, [
-        h(Breadcrumb, [
-          h(Link, { href: Route('open-backend') }, 'Backends'),
-          h(Link, {
-            href: Route('backend', {
-              backendID: backend.type.asIdentifier(),
-            })
-          }, backend.metadata.label),
-
-          'View authority'
-        ]),
-
         h(Source, { source: authority.source }),
-
       ])
     )
   }
