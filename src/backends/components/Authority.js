35425781d6ec127261c3e9f2088b50060c7a558c
Patrick Golden <ptgolden0@gmail.com>
2017-07-05 11:12:54 -0400

    Get rid of modal period editing

--- src/backends/components/Authority.js
+++ src/backends/components/Authority.js
@@ -1,54 +1,10 @@
 "use strict";
 
 const h = require('react-hyperscript')
-    , React = require('react')
     , { Box } = require('axs-ui')
-    , { Source, Button } = require('lib/ui')
+    , { Source } = require('lib/ui')
 
-const ViewAuthority = props =>
+module.exports =  props =>
   h(Box, [
-    h(Button, {
-      onClick: props.toggleForm,
-    }, 'Add period'),
-
-    h(Button, {
-      onClick: props.toggleForm,
-    }, 'Edit authority'),
     h(Source, { source: props.authority.source }),
   ])
-
-const EditPeriod = props =>
-  h(Box, [
-    h('h1', 'Editor the preiod'),
-    h(Button, {
-      onClick: props.toggleForm,
-    }, 'Cancel'),
-  ])
-
-module.exports = class Authority extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      addingPeriod: false,
-      period: {},
-    }
-  }
-
-  render() {
-    const { addingPeriod } = this.state
-        , { authority, id } = this.props
-
-    const childProps = {
-      authority,
-      toggleForm: () => this.setState(prev => ({ addingPeriod: !prev.addingPeriod  })),
-      handleSavePeriod: period => {
-        period;
-      }
-    }
-
-    const Component = addingPeriod ? EditPeriod : ViewAuthority;
-
-    return h(Component, childProps)
-  }
-}
