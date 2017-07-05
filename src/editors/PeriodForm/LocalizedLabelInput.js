859c2c51a84a2e91c5266de378938cf7b80c8e1f
Patrick Golden <ptgolden0@gmail.com>
2017-07-05 14:45:34 -0400

    Get localized label form elements working properly

--- src/editors/PeriodForm/LocalizedLabelInput.js
+++ src/editors/PeriodForm/LocalizedLabelInput.js
@@ -7,7 +7,7 @@ const h = require('react-hyperscript')
     , languages = require('lib/util/languages').getSortedList()
     , scripts = require('lib/util/scripts').getSortedList()
     , { Input, Flex, Box } = require('axs-ui')
-    , { DropdownMenu, Button$Primary } = require('lib/ui')
+    , { DropdownMenuButton, DropdownMenuMenu, Button$Primary } = require('lib/ui')
 
 const getSuggestions = list => value => {
   const input = value.trim()
@@ -22,6 +22,7 @@ class Suggestor extends React.Component {
     super();
 
     this.state = {
+      editing: false,
       value: '',
       suggestions: [],
     }
@@ -29,63 +30,96 @@ class Suggestor extends React.Component {
 
   render() {
     const { editing, value, suggestions } = this.state
+        , { onSelect } = this.props
 
     return (
       h(Box, { mr: 1, css: { position: 'relative' }}, [
-        h(DropdownMenu, {
+        h(DropdownMenuButton, {
           label: this.props.value,
-        }, h(Box, { p: 1, width: 600, }, [
-          h(Autosuggest, {
-            suggestions,
-            highlightFirstSuggestion: true,
-            getSuggestionValue: R.prop('name'),
-            onSuggestionsFetchRequested: e => this.setState({
-              suggestions: getSuggestions(this.props.items)(e.value)
-            }),
-            onSuggestionsClearRequested: e => this.setState({
-              suggestions: [],
-            }),
-            theme: {
-              suggestionsList: {
-                margin: 0,
-                padding: 0,
-                listStyleType: 'none',
-              },
+          isOpen: editing,
+          onClick: () => this.setState(prev => ({ editing: !prev.editing }))
+        }),
 
-              suggestionsContainerOpen: {
-                marginTop: '1em',
-                background: 'white',
-                height: 200,
-                overflowY: 'scroll',
-                zIndex: 1,
-              },
-            },
+        editing && h('div', {
+          ref: el => {
+            if (el) {
+              const input = el.querySelector('input')
 
-            renderInputComponent: props => h(Input, props),
-
-            renderSuggestion: (item, { isHighlighted }) =>
-              h(Box, Object.assign({
-                px: 1,
-                py: '6px',
-                border: 1,
-                borderColor: 'transparent',
-                css: {
-                  borderRadius: 1,
-                  ':hover': {
-                    cursor: 'pointer',
-                  }
-                }
-              }, isHighlighted && { bg: 'gray2' }), [
-                item.name
-              ]),
-
-            inputProps: {
-              value,
-              placeholder: 'Begin typing to search',
-              onChange: (e, { newValue }) => this.setState({ value: newValue }),
+              if (input && !input.focused) {
+                input.focus();
+                input.focused = true;
+              }
             }
-          })
-        ]))
+          }
+        }, [
+          h(DropdownMenuMenu, { p: 1, width: 600, }, [
+            h(Autosuggest, {
+              suggestions,
+              highlightFirstSuggestion: true,
+              focusInputOnSuggestionClick: false,
+              getSuggestionValue: R.prop('name'),
+              onSuggestionsFetchRequested: e => this.setState({
+                suggestions: getSuggestions(this.props.items)(e.value)
+              }),
+              onSuggestionsClearRequested: () => this.setState({
+                suggestions: [],
+              }),
+              onSuggestionSelected: (e, { suggestion }) => {
+                onSelect(suggestion);
+
+                this.setState({
+                  editing: false,
+                  suggestions: [],
+                  value: ''
+                })
+              },
+              theme: {
+                suggestionsList: {
+                  margin: 0,
+                  padding: 0,
+                  listStyleType: 'none',
+                },
+
+                suggestionsContainerOpen: {
+                  marginTop: '1em',
+                  background: 'white',
+                  height: 200,
+                  overflowY: 'scroll',
+                  zIndex: 1,
+                },
+              },
+
+              renderInputComponent: props => h(Input, props),
+
+              renderSuggestion: (item, { isHighlighted }) =>
+                h(Box, Object.assign({
+                  px: 1,
+                  py: '6px',
+                  border: 1,
+                  borderColor: 'transparent',
+                  css: {
+                    borderRadius: 1,
+                    ':hover': {
+                      cursor: 'pointer',
+                    }
+                  }
+                }, isHighlighted && { bg: 'gray2' }), [
+                  item.name
+                ]),
+
+              inputProps: {
+                value,
+                placeholder: 'Begin typing to search',
+                onChange: (e, { newValue }) => this.setState({ value: newValue }),
+                onBlur: () => this.setState({
+                  editing: false,
+                  suggestions: [],
+                  value: ''
+                })
+              }
+            })
+          ])
+        ])
       ])
     )
   }
@@ -103,32 +137,18 @@ module.exports = ({
       h(Suggestor, {
         items: languages,
         value: label.language,
+        onSelect: val => {
+          onValueChange(R.assoc('language', val.code.toLowerCase(), label))
+        },
       }),
 
       h(Suggestor, {
         items: scripts,
         value: label.script,
-      }),
-
-      /*
-      h(Autocomplete, {
-        value: label.script,
-        items: scripts.getSortedList(),
-        getItemValue: script => script.name,
-        onSelect: (str, val) => {
-          onValueChange(R.assoc('script', val.code, label))
+        onSelect: val => {
+          onValueChange(R.assoc('script', val.code.toLowerCase(), label))
         },
-        renderItem: item => h('div', item.name),
-        inputProps: {
-          style: {
-            marginRight: '2px',
-            width: '50px',
-            textAlign: 'center',
-            cursor: 'pointer',
-          }
-        }
       }),
-      */
 
       h(Input, {
         id,
