7ef0d9d0b06779b2c4ae79a25bea970fc3e1b65f
Patrick Golden <ptgolden0@gmail.com>
2017-09-07 13:13:34 -0400

    No need for two providers
    org-shell takes care of store and all that

--- modules/periodo-app/src/index.js
+++ modules/periodo-app/src/index.js
@@ -3,8 +3,6 @@
 const h = require('react-hyperscript')
     , ReactDOM = require('react-dom')
     , fastclick = require('fastclick')
-    , { Provider } = require('react-redux')
-    , createStore = require('./store')
     , Application = require('./main/Application')
 
 
@@ -12,15 +10,7 @@ global.CORS_PROXY_URL = 'https://ptgolden.org/cors-anywhere/'
 global.CORS_PROXY_ENABLED = true
 
 if (process.browser) {
-  const store = createStore()
-
   fastclick(document.body);
 
-  const component = (
-    h(Provider, { store }, [
-      h(Application)
-    ])
-  )
-
-  ReactDOM.render(component, document.getElementById('main'))
+  ReactDOM.render(h(Application), document.getElementById('main'))
 }
