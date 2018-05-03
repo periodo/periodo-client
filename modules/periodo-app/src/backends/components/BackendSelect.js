0020e8d8cd10061c871407faa7031be76dec6650
Patrick Golden <ptgolden0@gmail.com>
2018-05-03 12:24:01 -0400

    Space out backend selection table

--- modules/periodo-app/src/backends/components/BackendSelect.js
+++ modules/periodo-app/src/backends/components/BackendSelect.js
@@ -6,35 +6,62 @@ const h = require('react-hyperscript')
     , { Route } = require('org-shell')
 
 module.exports = props =>
-  h(Box, [
-    h('table', [
-      h('thead', [
-        h('tr', [
-          h('td', 'Type'),
-          h('td', 'Label'),
-          h('td', 'Description'),
-          h('td', 'Last opened'),
-        ])
-      ]),
-
-      h('tbody', props.backends.map(backend =>
-        h('tr', { key: backend.storage.url || backend.storage.id }, [
-          h('td', backend.storage.case({
-            Web: () => 'Web',
-            IndexedDB: () => 'Local',
-            Memory: () => 'Memory',
-            Canonical: () => 'Web',
-          })),
-          h('td', [
-            h(Link, {
-              route: Route('backend-home', {
-                backendID: backend.asIdentifier(),
-              })
-            }, backend.metadata.label)
-          ]),
-          h('td', backend.metadata.description),
-          h('td', {}, new Date(backend.metadata.accessed).toLocaleDateString()),
-        ])
-      ))
+  h(Box, {
+    is: 'table',
+    css: {
+      width: '100%',
+      borderSpacing: '4px 0',
+    }
+  }, [
+    h('thead', [
+      h('tr', [
+        h(Box, {
+          is: 'td',
+          css: {
+            fontWeight: 'bold',
+          },
+        }, 'Type'),
+        h(Box, {
+          is: 'td',
+          css: {
+            fontWeight: 'bold',
+          },
+        }, 'Label'),
+        h(Box, {
+          is: 'td',
+          css: {
+            fontWeight: 'bold',
+          },
+        }, 'Description'),
+        h(Box, {
+          is: 'td',
+          css: {
+            fontWeight: 'bold',
+          },
+        }, 'Last opened'),
+      ])
     ]),
+
+    h('tbody', props.backends.map(backend =>
+      h('tr', { key: backend.storage.url || backend.storage.id }, [
+        h('td', backend.storage.case({
+          Web: () => 'Web',
+          IndexedDB: () => 'Local',
+          Memory: () => 'Memory',
+          Canonical: () => 'Web',
+        })),
+        h('td', [
+          h(Link, {
+            route: Route('backend-home', {
+              backendID: backend.asIdentifier(),
+            })
+          }, backend.metadata.label)
+        ]),
+        h('td', backend.metadata.description),
+        h(Box, {
+          is: 'td',
+          padding: '4px 0',
+        }, new Date(backend.metadata.accessed).toLocaleDateString()),
+      ])
+    ))
   ])
