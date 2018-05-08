05d90ca03997ef561c8c34d21463c34ba6a76372
Patrick Golden <ptgolden0@gmail.com>
2018-05-08 13:54:10 -0400

    Better table styling for choosing an existing backend

--- modules/periodo-app/src/backends/components/BackendSelect.js
+++ modules/periodo-app/src/backends/components/BackendSelect.js
@@ -1,51 +1,60 @@
 "use strict";
 
 const h = require('react-hyperscript')
-    , { Link } = require('periodo-ui')
-    , { Box } = require('periodo-ui')
+    , { Box, ResourceTitle, Link } = require('periodo-ui')
     , { Route } = require('org-shell')
+    , { themeGet } = require('styled-system')
 
 const Table = Box.withComponent('table').extend`
   width: 100%;
   border-spacing: 4px 0;
+  border-collapse: collapse;
 
-  & tbody td {
-    padding: 4px 0;
+  border: 1px solid ${themeGet('colors.gray.1')};
+
+  & td, & th {
+    padding: 6px;
+    border-bottom: 1px solid ${themeGet('colors.gray.1')};
   }
 
   & th {
+    background-color: ${themeGet('colors.gray.1')};
+    text-align: left;
     font-weight: bold;
   }
 `
 
 module.exports = props =>
-  h(Table, [
-    h('thead', [
-      h('tr', [
-        h('th', 'Type'),
-        h('th', 'Label'),
-        h('th', 'Description'),
-        h('th', 'Last opened'),
-      ])
-    ]),
+  h(Box, [
+    h(ResourceTitle, 'Select backend'),
+    h(Table, {}, [
+      h('thead', [
+        h('tr', [
+          h('th', 'Type'),
+          h('th', 'Label'),
+          h('th', 'Description'),
+          h('th', 'Last opened'),
+        ])
+      ]),
 
-    h('tbody', props.backends.map(backend =>
-      h('tr', { key: backend.storage.url || backend.storage.id }, [
-        h('td', backend.storage.case({
-          Web: () => 'Web',
-          IndexedDB: () => 'Local',
-          Memory: () => 'Memory',
-          Canonical: () => 'Web',
-        })),
-        h('td', [
-          h(Link, {
-            route: Route('backend-home', {
-              backendID: backend.asIdentifier(),
-            })
-          }, backend.metadata.label)
-        ]),
-        h('td', backend.metadata.description),
-        h('td', {}, new Date(backend.metadata.accessed).toLocaleDateString()),
-      ])
-    ))
+      h('tbody', props.backends.map(backend =>
+        h('tr', { key: backend.storage.url || backend.storage.id }, [
+          h('td', backend.storage.case({
+            Web: () => 'Web',
+            IndexedDB: () => 'Local',
+            Memory: () => 'Memory',
+            Canonical: () => 'Web',
+          })),
+          h('td', [
+            h(Link, {
+              route: Route('backend-home', {
+                backendID: backend.asIdentifier(),
+              })
+            }, backend.metadata.label)
+          ]),
+          h('td', backend.metadata.description),
+          h('td', {}, new Date(backend.metadata.accessed).toLocaleDateString()),
+        ])
+      ))
+    ])
   ])
