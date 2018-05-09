f600f84b67b642a238c4f0cf6ce18eaa5c4937e4
Patrick Golden <ptgolden0@gmail.com>
2018-05-09 11:13:29 -0400

    Add missing "direction" prop to BackendPatch

--- modules/periodo-app/src/backends/components/BackendPatch.js
+++ modules/periodo-app/src/backends/components/BackendPatch.js
@@ -2,10 +2,12 @@
 
 const h = require('react-hyperscript')
     , Compare = require('../../patches/Compare')
+    , { PatchDirection } = require('../../patches/types')
 
 module.exports = function BackendPatch(props) {
   return (
     h(Compare, {
+      direction: PatchDirection.Pull,
       localDataset: props.extra.prevDataset,
       remoteDataset: props.extra.dataset,
     })
