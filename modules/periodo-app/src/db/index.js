0c675fdacb75b2ddead02b5dfe6517223ef864ed
Patrick Golden <ptgolden0@gmail.com>
2018-06-03 15:57:04 -0400

    Add env var to disable creating a default Canonical backend
    Default backend should be configurable in the future, but this was a
    way to get tests working as expected

--- modules/periodo-app/src/db/index.js
+++ modules/periodo-app/src/db/index.js
@@ -11,19 +11,21 @@ module.exports = function periodoDB(dexieOpts) {
   require('./version-03')(db)
 
   db.on('populate', function () {
-    db.remoteBackends.add({
-      label: 'Canonical',
-      description: 'The canonical PeriodO dataset',
-      url: 'https://test.perio.do/',
+    if (!process.env.NO_DEFAULT_BACKEND) {
+      db.remoteBackends.add({
+        label: 'Canonical',
+        description: 'The canonical PeriodO dataset',
+        url: 'https://test.perio.do/',
 
-      // FIXME: These are lies! But there's no way to know the latter two
-      // until a request to the server has actually been made, and the type
-      // definition says that they must be dates. It's not a big deal, but
-      // this is a note for later.
-      created: new Date(),
-      modified: new Date(),
-      accessed: new Date(),
-    })
+        // FIXME: These are lies! But there's no way to know the latter two
+        // until a request to the server has actually been made, and the type
+        // definition says that they must be dates. It's not a big deal, but
+        // this is a note for later.
+        created: new Date(),
+        modified: new Date(),
+        accessed: new Date(),
+      })
+    }
 
     db.settings.add({})
   })
