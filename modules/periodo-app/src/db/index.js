b78f9b3fe9e5a9e55181cc99e271ba07e2408c79
Patrick Golden <ptgolden0@gmail.com>
2018-08-14 09:34:33 -0400

    Seed default backends with global variable, not environment variable
    The latter is only able to be set at build time. We serve a built
    version of the client on NPM, so there needed to be some other way to
    set it.

--- modules/periodo-app/src/db/index.js
+++ modules/periodo-app/src/db/index.js
@@ -12,19 +12,17 @@ module.exports = function periodoDB(dexieOpts) {
   require('./version-04')(db)
 
   db.on('populate', function () {
-    if (!process.env.NO_DEFAULT_BACKEND) {
-      db.remoteBackends.add({
-        label: 'Canonical',
-        description: 'The canonical PeriodO dataset',
-        url: 'https://test.perio.do/',
-
-        // FIXME: These are lies! But there's no way to know the latter two
-        // until a request to the server has actually been made, and the type
-        // definition says that they must be dates. It's not a big deal, but
-        // this is a note for later.
-        created: new Date(),
-        modified: new Date(),
-        accessed: new Date(),
+    if (global.DEFAULT_PERIODO_BACKENDS) {
+      [].concat(global.DEFAULT_PERIODO_BACKENDS).forEach(backend => {
+        db.remoteBackends.add(Object.assign({}, backend, {
+          // FIXME: These are lies! But there's no way to know the latter two
+          // until a request to the server has actually been made, and the type
+          // definition says that they must be dates. It's not a big deal, but
+          // this is a note for later.
+          created: new Date(),
+          modified: new Date(),
+          accessed: new Date(),
+        }))
       })
     }
 
