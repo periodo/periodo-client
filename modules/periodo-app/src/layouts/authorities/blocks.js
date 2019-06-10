d4f539daf3873cc9df2d006ced537f5db8b1bb84
Patrick Golden <ptgolden0@gmail.com>
2019-06-10 15:41:51 -0400

    Rename file for timeline component

--- modules/periodo-app/src/layouts/authorities/blocks.js
+++ modules/periodo-app/src/layouts/authorities/blocks.js
@@ -5,6 +5,6 @@ module.exports = {
   'period-list': require('./PeriodList'),
   'authority-list': require('./AuthorityList'),
   'time-cutoff': require('./HumanTimeCheckmark'),
-  'timespan-visualization': require('./d3_test'),
+  'timespan-visualization': require('./Timeline'),
   'facets': require('./Facets')
 }
