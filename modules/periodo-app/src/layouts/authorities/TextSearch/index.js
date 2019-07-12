49e4edaa07a63861f9913bac00740aa2b7f12332
Patrick Golden <ptgolden0@gmail.com>
2019-07-12 15:29:44 -0400

    Improve text search layout block
    1) Allow toggling between using alternate labels or not. In the future
       there may be more options (e.g. full text search, including notes and
       such).
    
    2) Searching over alternate labels actually wasn't working before. Now
       it is.
    
    3) Improve styling of input element

--- modules/periodo-app/src/layouts/authorities/TextSearch/index.js
+++ modules/periodo-app/src/layouts/authorities/TextSearch/index.js
@@ -2,9 +2,90 @@
 
 const h = require('react-hyperscript')
     , R = require('ramda')
+    , React = require('react')
     , { alternateLabels } = require('periodo-utils/src/period')
+    , { Box, Flex, Input, Label } = require('periodo-ui')
+    , { RandomID } = require('periodo-common')
+    , styled = require('styled-components').default
 
-const _alternateLabels = R.memoizeWith(R.identity, alternateLabels)
+const Container = styled(Box)`
+input[type="radio"] {
+  margin: 0;
+  margin-right: 4px;
+}
+`
+
+const _alternateLabels = R.memoizeWith(p => p.id, alternateLabels)
+
+class Search extends React.Component {
+  toggleRadio(withAlternate) {
+    const { opts, updateOpts } = this.props
+        , nextOpts = Object.assign({}, opts)
+
+    if (withAlternate) {
+      nextOpts.withAlternate = true
+    } else {
+      nextOpts.withAlternate = undefined
+    }
+
+    const invalidate = !!opts.withAlternate !== !!nextOpts.withAlternate
+
+    updateOpts(nextOpts, invalidate)
+  }
+
+  render() {
+    const { opts, updateOpts, randomID } = this.props
+        , { text, withAlternate } = opts
+        , inputID = randomID('search')
+        , radioName = randomID('radio')
+
+    return (
+      h(Container, [
+        h(Label, { htmlFor: inputID }, 'Search'),
+
+        h(Flex, { mt: 1, mb: 2 }, [
+          h(Flex, { mr: 3 }, [
+            h('input', {
+              id: randomID('radio1'),
+              type: 'radio',
+              name: radioName,
+              checked: !withAlternate,
+              onChange: e => {
+                this.toggleRadio(false)
+              },
+            }),
+            h('label', { htmlFor: randomID('radio1') }, 'Labels'),
+          ]),
+
+          h(Flex, [
+            h('input', {
+              id: randomID('radio2'),
+              type: 'radio',
+              name: radioName,
+              checked: !!withAlternate,
+              onChange: e => {
+                this.toggleRadio(true)
+              },
+            }),
+            h('label', { htmlFor: randomID('radio2') }, 'Labels + alternate labels'),
+          ]),
+        ]),
+
+        h(Input, {
+          type: 'text',
+          id: inputID,
+          value: text || '',
+          onChange: e => {
+            const text = e.target.value
+                , invalidate = text.slice(-1) !== '|'
+
+            updateOpts(Object.assign({}, opts, { text }), invalidate)
+          }
+        }),
+      ])
+    )
+  }
+}
 
 module.exports = {
   label: 'Text search',
@@ -16,23 +97,18 @@ module.exports = {
 
     const regex = text && new RegExp(text, 'i')
 
-    return period => (
-      regex.test(period.label) ||
-      _alternateLabels(period).some(label => regex.test(label.value))
-    )
-  },
-  Component: props =>
-    h('label', [
-      'Search: ',
-      h('input', {
-        type: 'text',
-        value: props.opts.text || '',
-        onChange: e => {
-          const text = e.target.value
-              , invalidate = text.slice(-1) !== '|'
+    let test
 
-          props.updateOpts({ text }, invalidate)
-        }
-      }),
-    ])
+    if (!!opts.withAlternate) {
+      test = period => (
+        regex.test(period.label) ||
+        _alternateLabels(period).some(({ label }) => regex.test(label))
+      )
+    } else {
+      test = period => regex.test(period.label)
+    }
+
+    return test
+  },
+  Component: RandomID(Search),
 }
