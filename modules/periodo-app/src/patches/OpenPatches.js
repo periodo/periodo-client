c33a232bf351ce75252c17333259d1654928ff2a
Patrick Golden <ptgolden0@gmail.com>
2019-07-29 16:21:21 -0400

    Add toggle to only show open patches

--- modules/periodo-app/src/patches/OpenPatches.js
+++ modules/periodo-app/src/patches/OpenPatches.js
@@ -2,7 +2,7 @@
 
 const h = require('react-hyperscript')
     , { useState } = require('react')
-    , { Box, Text, Link } = require('periodo-ui')
+    , { Box, Text, Link, Label } = require('periodo-ui')
     , { Route } = require('org-shell')
     , PatchRequestLayout = require('../layouts/patch_requests')
 
@@ -14,12 +14,37 @@ type = request-list
 function ReviewPatches(props) {
   const { patchRequests, backend } = props
       , [ blockOpts, setBlockOpts ] = useState({})
+      , [ onlyOpen, setOnlyOpen ] = useState(false)
+
+  let shownPatchRequests = patchRequests
+
+  if (onlyOpen) {
+    shownPatchRequests = shownPatchRequests.filter(p => p.open)
+  }
 
   return (
     h(Box, [
+      h(Box, [
+        h(Label, {
+          style: {
+            display: 'flex',
+            alignItems: 'center',
+          },
+        }, [
+          h('input', {
+            type: 'checkbox',
+            checked: onlyOpen,
+            onChange: () => {
+              setOnlyOpen(!onlyOpen)
+            },
+          }),
+          'Open patches only',
+        ])
+      ]),
+
       h(PatchRequestLayout, {
         layout,
-        patchRequests,
+        patchRequests: shownPatchRequests,
         backend,
         blockOpts,
         onBlockOptsChange: setBlockOpts,
