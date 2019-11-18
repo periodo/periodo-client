df7f449fd2327059999edc86b7d9c4dd4abd32df
Patrick Golden <ptgolden0@gmail.com>
2019-11-18 15:40:02 -0500

    Only add periodo server URL for client if running in browser

--- modules/periodo-app/src/globals.js
+++ modules/periodo-app/src/globals.js
@@ -1,7 +1,16 @@
 "use strict";
 
+let periodoServerURL
+
+if (typeof window !== 'undefined') {
+  periodoServerURL = (
+    global.PERIODO_SERVER_URL ||
+    new URL('/', window.location).href
+  )
+}
+
 module.exports = {
-  periodoServerURL: global.PERIODO_SERVER_URL || '/',
+  periodoServerURL,
   permalinkURL: global.PERIODO_PERMALINK_URL,
   corsProxyURL: global.PERIODO_PROXY_URL,
   corsProxyEnabled: global.CORS_PROXY_ENABLED,
