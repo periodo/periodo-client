7395ceb80a728d6086ed24f5b5f850fc080e8e1f
Ryan Shaw <ryanshaw@unc.edu>
2020-04-25 21:13:32 -0400

    show explanation if adding fails due to name collision; styling

--- modules/periodo-app/src/backends/components/AddBackend.js
+++ modules/periodo-app/src/backends/components/AddBackend.js
@@ -1,43 +1,56 @@
 "use strict";
 
 const h = require('react-hyperscript')
-    , { Box, ResourceTitle } = require('periodo-ui')
+    , { useState } = require('react')
+    , { Box, SectionHeading, Section } = require('periodo-ui')
     , { BackendForm } = require('../../forms')
     , BackendAction = require('../actions')
     , { BackendStorage } = require('../types')
     , { handleCompletedAction } = require('org-async-actions')
 
-const AddBackend = props =>
-  h(Box, [
-    h(ResourceTitle, 'Add data source'),
-    h(BackendForm, {
-      handleSave: async state => {
-        const { label, description='', type, file } = state
+const AddBackend = props => {
 
-        let storage
+  const [ urlError, setURLError ] = useState(undefined)
 
-        if (type === 'Web') {
-          storage = BackendStorage.WebOf(state)
-        } else if (type === 'IndexedDB') {
-          storage = BackendStorage.IndexedDB(null)
-        } else if (type === 'StaticFile') {
-          storage = BackendStorage.StaticFile(null, file)
-        }
+  return h(Box, [
+    h(SectionHeading, 'Add data source'),
+    h(Section, [
+      h(BackendForm, {
+        urlError,
+        handleSave: async state => {
+          const { label, description='', type, file } = state
 
-        const resp = await props.dispatch(
-          BackendAction.CreateBackend(storage, label, description))
+          let storage
 
-        handleCompletedAction(
-          resp,
-          props.onSave || (() => null),
-          err => {
-            alert('Error saving data source');
-            // eslint-disable-next-line no-console
-            console.error(err);
+          if (type === 'Web') {
+            storage = BackendStorage.WebOf(state)
+          } else if (type === 'IndexedDB') {
+            storage = BackendStorage.IndexedDB(null)
+          } else if (type === 'StaticFile') {
+            storage = BackendStorage.StaticFile(null, file)
           }
-        )
-      },
-    }),
+
+          const resp = await props.dispatch(
+            BackendAction.CreateBackend(storage, label, description))
+
+          handleCompletedAction(
+            resp,
+            props.onSave || (() => null),
+            err => {
+              if (err.message &&
+                  err.message === 'Key already exists in the object store.') {
+                setURLError('There is already a data source with this URL.')
+              } else {
+                alert('Error saving data source')
+                // eslint-disable-next-line no-console
+                console.error(err)
+              }
+            }
+          )
+        },
+      }),
+    ]),
   ])
+}
 
 module.exports = AddBackend;
