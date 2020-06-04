8e5dea2b69ef25588caef00c5203cdf68f3bd9f4
Ryan Shaw <ryanshaw@unc.edu>
2020-06-04 10:24:20 -0400

    fix bug when earliest year or latest year is zero

--- modules/periodo-utils/src/terminus_list.js
+++ modules/periodo-utils/src/terminus_list.js
@@ -5,9 +5,8 @@ const { earliestYear, latestYear } = require('./terminus')
 // Iterable<Terminus> -> Object({ label: String, iso: Int }) or Null
 function maxYear(termini) {
   const latest = termini.reduce((prev, terminus) => {
-    const iso = latestYear(terminus) || -Infinity
-
-    return iso > prev.iso
+    const iso = latestYear(terminus)
+    return iso !== null && iso > prev.iso
       ?
       {
         terminus,
@@ -30,9 +29,8 @@ function maxYear(termini) {
 // Iterable<Terminus> -> Object({ label: String, iso: Int }) or Null
 function minYear(termini) {
   const earliest = termini.reduce((prev, terminus) => {
-    const iso = earliestYear(terminus) || Infinity
-
-    return iso < prev.iso
+    const iso = earliestYear(terminus)
+    return iso !== null && iso < prev.iso
       ?
       {
         terminus,
