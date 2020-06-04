2d30a63483f1d8456e96142b1a81aa25810ff71c
Patrick Golden <ptgolden0@gmail.com>
2020-06-04 14:09:13 -0400

    Adapt form elements to rebass

--- modules/periodo-app/src/main/Test.js
+++ modules/periodo-app/src/main/Test.js
@@ -4,6 +4,14 @@ const h = require('react-hyperscript')
     , ui = require('periodo-ui')
     , { ThemeProvider } = require('emotion-theming')
 
+function Space() {
+  return (
+    h(ui.Box, {
+      my: 4,
+    })
+  )
+}
+
 const UISection = props =>
   h(ui.Box, {
     sx: {
@@ -32,16 +40,63 @@ module.exports = function Test() {
           h(ui.HelpText, 'Some help text'),
         ]),
       ]),
+
       h(UISection, [
         h(ui.Alert$Success, { my: 2 }, 'Success'),
         h(ui.Alert$Warning, { my: 2 }, 'Warning'),
         h(ui.Alert$Error, { my: 2 }, 'Error'),
       ]),
+
       h(UISection, [
         h(ui.Button$Primary, { m: 2 }, 'Primary button'),
         h(ui.Button$Danger, { m: 2 }, 'Danger button'),
         h(ui.Button$Default, { m: 2 }, 'Default button'),
         h(ui.AriaButton, { m: 2 }, 'Aria (fake) button'),
+      ]),
+
+      h(UISection, [
+        h(ui.Box, {
+          p: 2,
+          width: '300px',
+        }, [
+          h(ui.Input, {
+            defaultValue: 'input',
+          }),
+
+          h(ui.Input, {
+            defaultValue: 'input disabled',
+            disabled: true,
+          }),
+
+          h(Space),
+
+          h(ui.Select, {
+          }, [
+            h('option', 'Apples'),
+            h('option', 'Oranges'),
+            h('option', 'Bananas'),
+          ]),
+
+          h(ui.Select, {
+            disabled: true,
+          }, [
+            h('option', 'Apples'),
+            h('option', 'Oranges'),
+            h('option', 'Bananas'),
+          ]),
+
+          h(Space),
+
+          h(ui.Textarea, {
+            defaultValue: 'textarea',
+          }),
+          h(ui.Textarea, {
+            disabled: true,
+            defaultValue: 'textarea disabled',
+          }),
+
+          h(Space),
+        ]),
       ]),
     ])
   )
--- modules/periodo-ui/src/components/FormElements.js
+++ modules/periodo-ui/src/components/FormElements.js
@@ -1,94 +1,117 @@
 "use strict";
 
 const h = require('react-hyperscript')
-    , R = require('ramda')
-    , sys = require('system-components').default
-    , { themeGet } = require('styled-system')
     , { Box } = require('./Base')
     , { Alert$Error } = require('./Alerts')
 
-const disabled = props => ({
+const disabledStyle = {
   ':disabled': {
     cursor: 'not-allowed',
-    backgroundColor: themeGet('colors.gray.3')(props),
+    backgroundColor: 'gray.3',
     color: 'gray.9',
   },
-})
+}
 
-exports.Select = sys({
-  is: 'select',
-  bg: 'gray.1',
-  border: 'none',
-  borderRadius: 0,
-  display: 'block',
-  fontFamily: 'sans-serif',
-  fontSize: 1,
-  height: 36,
-  pl: 2,
-  pr: 4,
-  css: {
-    appearance: 'none',
-    backgroundImage: "url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23000000%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E')",
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'right .7em top 50%',
-    backgroundSize: '.65em auto',
-  },
-}, 'minWidth', 'maxWidth', disabled)
+const borderStyle = {
+  border: '1px solid #fff',
+  borderColor: 'gray.5',
+}
 
-exports.Textarea = sys({
-  is: 'textarea',
-  p: 2,
-  border: 'none',
-  bg: 'gray.1',
-  width: '100%',
-  fontFamily: 'sans-serif',
-  fontSize: 1,
-}, 'minWidth', 'maxWidth', disabled)
+exports.Select = props =>
+  h(Box, {
+    as: 'select',
+    sx: {
+      bg: 'white',
+      fontFamily: 'sans-serif',
+      fontSize: 1,
+      height: '36px',
+      pl: 2,
+      pr: 4,
 
-exports.Input = sys({
-  is: 'input',
-  p: 2,
-  m: 0,
-  fontSize: 1,
-  width: '100%',
-  border: 'none',
-  bg: 'gray.1',
-}, 'minWidth', 'maxWidth', disabled)
+      // This is a triangle
+      appearance: 'none',
+      backgroundImage: "url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23000000%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E')",
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'right .7em top 50%',
+      backgroundSize: '.65em auto',
 
-exports.Checkbox = props => h('input', {
-  type: 'checkbox',
-  ...props,
-})
+      ...borderStyle,
+      ...disabledStyle,
+    },
+    ...props,
+  })
 
-exports.Errors = props =>
+exports.Textarea = props =>
+  h(Box, {
+    as: 'textarea',
+    sx: {
+      p: 2,
+      width: '100%',
+      fontFamily: 'sans-serif',
+      fontSize: 1,
+
+      ...borderStyle,
+      ...disabledStyle,
+    },
+    ...props,
+  })
+
+exports.Input = props =>
+  h(Box, {
+    as: 'input',
+    sx: {
+      p: 2,
+      m: 0,
+      fontSize: 1,
+      width: '100%',
+
+      ...borderStyle,
+      ...disabledStyle,
+    },
+    ...props,
+  })
+
+// FIXME: Need anything more here?
+exports.Checkbox = props =>
+  h(Box, {
+    as: 'input',
+    type: 'checkbox',
+    ...props,
+  })
+
+exports.Errors = ({ errors, ...props }) =>
   h(Alert$Error, {
     mb: 1,
-    ...R.omit([ 'errors' ], props),
-  },
-  h(Box, {
-    is: 'ul',
-    css: { listStyleType: 'none' },
-  }, props.errors.map((message, i) =>
+    ...props,
+  }, [
     h(Box, {
-      is: 'li',
-      mt: i > 0 ? 1 : 0,
-    }, message)
-  ))
-  )
+      as: 'ul',
+      sx: {
+        listStyleType: 'none',
+      },
+    }), errors.map((message, i) =>
+      h(Box, {
+        as: 'li',
+        mt: i > 0 ? 1 : 0,
+      })
+    ),
+  ])
 
-exports.Label = sys({
-  is: 'label',
-  blacklist: [ 'isRequired' ],
-  fontSize: 2,
-  color: 'black',
-  display: 'inline-block',
-  fontWeight: 'bold',
-}, props => ({
-  ':hover': {
-    cursor: props.htmlFor ? 'pointer' : 'auto',
-  },
-  ':after': !props.isRequired ? {} : {
-    content: '" \x2a        "',
-    color: 'red',
-  },
-}))
+exports.Label = ({ isRequired, ...props }) =>
+  h(Box, {
+    as: 'label',
+    sx: {
+      fontSize: 2,
+      color: 'black',
+      display: 'inline-block',
+      fontWeight: 'bold',
+      ':hover': !props.htmlFor ? {} : {
+        cursor: 'pointer',
+      },
+      ':after': !isRequired ? {} : {
+        content: '" \x2a        "',
+        color: 'red',
+      },
+    },
+    ...props,
+  })
