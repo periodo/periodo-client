87314880d3e2a760b100185360ada1864c51d085
Patrick Golden <ptgolden0@gmail.com>
2020-06-10 12:53:12 -0400

    Make ListBlock styling consistent with other tables

--- modules/periodo-app/src/layouts/ListBlock/ListRow.js
+++ modules/periodo-app/src/layouts/ListBlock/ListRow.js
@@ -14,53 +14,48 @@ function ListRow({
   itemViewRoute,
   itemEditRoute,
 }) {
-  return h(Box, {
-    as: 'tr',
-    key: item.id,
-    m: 0,
-    bg: 'gray.1',
-    css: {
-      ':hover': { backgroundColor: 'white' },
-    },
-  }, [
-    h(Box, {
-      as: 'td',
-      key: '_numbering',
-      p: 2,
-      verticalAlign: 'middle',
-      css: { whiteSpace: 'nowrap' },
+  return (
+    h('tr', {
+      key: item.id,
     }, [
-      h(Text, {
-        display: 'inline-block',
-        color: 'gray.6',
-        width: '4ch',
-        textAlign: 'right',
-      }, index + 1 + start),
+      h('td', {
+        key: '_numbering',
+        style: {
+          verticalAlign: 'middle',
+          whiteSpace: 'nowrap',
+        },
+      }, [
+        h(Text, {
+          display: 'inline-block',
+          width: '4ch',
+          textAlign: 'right',
+        }, index + 1 + start),
 
-      h(Link, {
-        ml: 2,
-        fontWeight: 100,
-        route: itemViewRoute(item, { backend }),
-      }, 'view'),
+        h(Link, {
+          ml: 2,
+          fontWeight: 100,
+          route: itemViewRoute(item, { backend }),
+        }, 'view'),
 
-      backend.isEditable() && itemEditRoute
-        ? (
-          h(Link, {
-            ml: 2,
-            fontWeight: 100,
-            route: itemEditRoute(item, { backend }),
-          }, 'edit')
-        )
-        : null,
-    ]),
-  ].concat(R.values(R.pick(shownColumns, columns)).map(
-    col => h(Box, {
-      as: 'td',
-      key: col.label,
-      p: 2,
-      verticalAlign: 'middle',
-    }, (col.render || R.identity)(col.getValue(item)))
-  )))
+        backend.isEditable() && itemEditRoute
+          ? (
+            h(Link, {
+              ml: 2,
+              fontWeight: 100,
+              route: itemEditRoute(item, { backend }),
+            }, 'edit')
+          )
+          : null,
+      ]),
+    ].concat(R.values(R.pick(shownColumns, columns)).map(
+      col => h(Box, {
+        as: 'td',
+        key: col.label,
+        p: 2,
+        verticalAlign: 'middle',
+      }, (col.render || R.identity)(col.getValue(item)))
+    )))
+  )
 }
 
 module.exports = ListRow
