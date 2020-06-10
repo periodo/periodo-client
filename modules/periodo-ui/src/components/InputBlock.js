periodo/periodo-client

commit 989bdd98ec1b9cdac8dc847bc2e5364e71be5762

Rename Span -> InlineText

modules/periodo-ui/src/components/Typography.js
@@ -1,10 +1,10 @@
 "use strict";
 
 const h = require('react-hyperscript')
-    , { Span } = require('./Base')
+    , { InlineText } = require('./Base')
 
 exports.Italic = props =>
-  h(Span, {
+  h(InlineText, {
     sx: {
       fontStyle: 'italic',
     },
@@ -12,7 +12,7 @@ exports.Italic = props =>
   })
 
 exports.InfoText = props =>
-  h(Span, {
+  h(InlineText, {
     sx: {
       fontStyle: 'italic',
       color: 'gray.8',
@@ -21,7 +21,7 @@ exports.InfoText = props =>
   })
 
 exports.WarnText = props =>
-  h(Span, {
+  h(InlineText, {
     sx: {
       fontStyle: 'italic',
       color: 'red.5',

modules/periodo-app/src/auth/components/ORCID.js
@@ -3,7 +3,7 @@
 const h = require('react-hyperscript')
     , React = require('react')
     , BackendAction = require('../../backends/actions')
-    , { Box, Span, Alert } = require('periodo-ui')
+    , { Box, InlineText, Alert } = require('periodo-ui')
     , { Link } = require('periodo-ui')
     , { connect } = require('react-redux')
 
@@ -97,13 +97,13 @@ class SignIn extends React.Component {
               height: '24',
               alt: 'ORCID logo',
             }),
-            h(Span, { ml: 1 }, 'Log in with your ORCID'),
+            h(InlineText, { ml: 1 }, 'Log in with your ORCID'),
           ])
         ),
 
         !!orcidCredential && (
           h(Box, [
-            h(Span, `Currently signed in as ${orcidCredential.name}.`),
+            h(InlineText, `Currently signed in as ${orcidCredential.name}.`),
             h(Link, {
               ml: 2,
               href: '',

modules/periodo-app/src/backends/components/AuthorityView.js
@@ -4,7 +4,7 @@ const h = require('react-hyperscript')
     , R = require('ramda')
     , React = require('react')
     , { Route } = require('org-shell')
-    , { Heading, Box, Span, Link, DownloadValue } = require('periodo-ui')
+    , { Heading, Box, InlineText, Link, DownloadValue } = require('periodo-ui')
     , { PermalinkValue, Section, EditorialNote  } = require('periodo-ui')
     , { Breadcrumb } = require('periodo-ui')
     , { HelpText  } = require('periodo-ui')
@@ -103,7 +103,7 @@ module.exports = class AuthorityLayout extends React.Component {
         }, description.source),
 
         h(Box, { mb:2 }, [
-          h(Span, {
+          h(InlineText, {
             fontSize: 3,
             color: 'gray.6',
           }, 'Permalink '),
@@ -118,7 +118,7 @@ module.exports = class AuthorityLayout extends React.Component {
           mb: description.editorialNote ? 2 : 0,
           fontSize: 3,
         }, [
-          h(Span, { color: 'gray.6' }, 'Download '),
+          h(InlineText, { color: 'gray.6' }, 'Download '),
           h(DownloadValue, {
             value: description.id,
             includeCSV: true,

modules/periodo-app/src/backends/components/PeriodView.js
@@ -4,7 +4,7 @@ const h = require('react-hyperscript')
     , R = require('ramda')
     , React = require('react')
     , { Route } = require('org-shell')
-    , { Box, Span, Heading, Text, Section, Breadcrumb } = require('periodo-ui')
+    , { Box, InlineText, Heading, Text, Section, Breadcrumb } = require('periodo-ui')
     , { Link, PermalinkValue, Note, EditorialNote } = require('periodo-ui')
     , { RelatedAuthorityValue, DownloadValue } = require('periodo-ui')
     , util = require('periodo-utils')
@@ -85,7 +85,7 @@ module.exports = class PeriodLayout extends React.Component {
         }, [
           `${ labelOf(period.start) } – ${ labelOf(period.stop) }`,
           period.spatialCoverageDescription
-            ? h(Span, { color: 'gray.4' }, ' | ')
+            ? h(InlineText, { color: 'gray.4' }, ' | ')
             : null,
           `${ period.spatialCoverageDescription || '' }`,
         ]),
@@ -94,7 +94,7 @@ module.exports = class PeriodLayout extends React.Component {
           mb: 2,
           fontSize: 3,
         }, [
-          h(Span, { color: 'gray.6' }, 'Defined by '),
+          h(InlineText, { color: 'gray.6' }, 'Defined by '),
           h(RelatedAuthorityValue, { value: authority }),
         ]),
 
@@ -102,14 +102,14 @@ module.exports = class PeriodLayout extends React.Component {
           mb: 2,
           fontSize: 3,
         }, [
-          h(Span, { color: 'gray.6' }, 'Permalink '),
+          h(InlineText, { color: 'gray.6' }, 'Permalink '),
           h(PermalinkValue, { value: period.id }),
         ]),
 
         h(Box, {
           fontSize: 3,
         }, [
-          h(Span, { color: 'gray.6' }, 'Download '),
+          h(InlineText, { color: 'gray.6' }, 'Download '),
           h(DownloadValue, { value: period.id }),
         ]),
 

modules/periodo-app/src/forms/PeriodForm/RelatedPeriodList.js
@@ -4,7 +4,7 @@ const h = require('react-hyperscript')
     , R = require('ramda')
     , { Route } = require('org-shell')
     , { RandomID } = require('periodo-common')
-    , { Box, Link, Text, HelpText, Label, Span, Tag } = require('periodo-ui')
+    , { Box, Link, Text, HelpText, Label, InlineText, Tag } = require('periodo-ui')
     , { Autosuggest, BackendContext } = require('periodo-ui')
     , util = require('periodo-utils')
     , { useContext } = require('react')
@@ -91,7 +91,7 @@ const renderSuggestion = (item, { isHighlighted, isSelected }) => h(Box,
     },
     ...isHighlighted && { bg: 'gray.2' },
   }, [
-    h(Span, {
+    h(InlineText, {
       display: 'inline-block',
       width: '1em',
       color: 'gray.6',

modules/periodo-app/src/layouts/authorities/AuthorityList.js
@@ -4,7 +4,7 @@ const h = require('react-hyperscript')
     , contributorList = require('periodo-utils/src/contributor_list')
     , source = require('periodo-utils/src/source')
     , ListBlock = require('../ListBlock')
-    , { Span, Text } = require('periodo-ui')
+    , { InlineText, Text } = require('periodo-ui')
     , { Route } = require('org-shell')
 
 const columns = {
@@ -19,7 +19,7 @@ const columns = {
 
       return list.length
         ? contributorList.asString(list)
-        : h(Span, {
+        : h(InlineText, {
           color: 'gray',
         }, '(not given)')
     },

modules/periodo-app/src/layouts/authorities/CoverageMap/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const h = require('react-hyperscript')
-    , { Label, HelpText, Span, LabeledMap } = require('periodo-ui')
+    , { Label, HelpText, InlineText, LabeledMap } = require('periodo-ui')
 
 const featuresOf = (period, gazetteers) => {
   if (!period) return []
@@ -37,7 +37,7 @@ const CoverageMap = ({
       ...(selectedPeriod
         ? [
           ', with the selected period in ',
-          h(Span, { color: '#ff0000' }, 'red'),
+          h(InlineText, { color: '#ff0000' }, 'red'),
         ]
         : []
       ),

modules/periodo-app/src/layouts/authorities/Timeline/index.js
@@ -2,7 +2,7 @@
 
 const h = require('react-hyperscript')
     , AutoSizer = require('react-virtualized-auto-sizer')
-    , { Label, HelpText, Span } = require('periodo-ui')
+    , { Label, HelpText, InlineText } = require('periodo-ui')
     , Plot = require('./Plot')
 
 function Timeline({
@@ -22,7 +22,7 @@ function Timeline({
       ...(selectedPeriod
         ? [
           ', with the selected period in ',
-          h(Span, { color: '#ff0000' }, 'red'),
+          h(InlineText, { color: '#ff0000' }, 'red'),
         ]
         : []
       ),

commit 7287f1be9afaead24d17146f49a8611594973e5c

Fix broken imports caused by 989bdd9 and f50727a

modules/periodo-ui/src/components/InputBlock.js
@@ -3,7 +3,8 @@
 const h = require('react-hyperscript')
     , R = require('ramda')
     , { RandomID } = require('periodo-common')
-    , { Box, HelpText } = require('./Base')
+    , { Box } = require('./Base')
+    , { HelpText } = require('./Typography')
     , { Label, Input, Textarea, Select } = require('./FormElements')
     , { Alert } = require('./Alerts')
 

modules/periodo-ui/src/components/Typography.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const h = require('react-hyperscript')
-    , { InlineText } = require('./Base')
+    , { InlineText, Text } = require('./Base')
 
 exports.Italic = props =>
   h(InlineText, {