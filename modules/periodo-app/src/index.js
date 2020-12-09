ccdd0bad32782983964f1e7e11c8e40be30a3f7d
Ryan Shaw <ryanshaw@unc.edu>
2020-12-09 08:48:27 -0500

    fastclick has no benefit on new browsers and risks introducing bugs

--- modules/periodo-app/src/index.js
+++ modules/periodo-app/src/index.js
@@ -8,11 +8,8 @@ Object.defineProperty(require('react'), 'PropTypes', {
 
 const h = require('react-hyperscript')
     , ReactDOM = require('react-dom')
-    , fastclick = require('fastclick')
     , Application = require('./main/Application')
 
 if (process.browser) {
-  fastclick(document.body);
-
   ReactDOM.render(h(Application), document.getElementById('main'))
 }
